d4cf6667cc145cc29179448c7f91cc308a557d14
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-05 01:47:51 +0900

    [MOVE] Extension files
    CommonClass 폴더 내에 Extension 파일이 존재하였음
    프로젝트 내에 보이는 Extension 폴더는 finder 상에 보이지 않음
    -> 파일 삭제 후 다시 붙여 재설정하였음

--- PLUB/Configuration/Extensions/Ex+UIColor.swift
+++ PLUB/Configuration/Extensions/Ex+UIColor.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 extension UIColor {
-  // MARK: hex code를 이용하여 정의
-  // ex. UIColor(hex: 0xF5663F)
+  
+  /// 16진수(hex code)를 이용하여 색상을 지정합니다.
+  ///
+  /// ```
+  /// let color: UIColor = UIColor(hex: 0xF5663F)
+  /// ```
+  /// - Parameters:
+  ///   - hex: 16진수의 Unsigned Int 값
+  ///   - alpha: 투명도를 설정합니다. 0과 1사이의 값을 가져야합니다.
   convenience init(hex: UInt, alpha: CGFloat = 1.0) {
     self.init(
       red: CGFloat((hex & 0xFF0000) >> 16) / 255.0,
