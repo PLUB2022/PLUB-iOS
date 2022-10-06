7acd101c290a4d53b19d28998a842403448be167
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-06 17:54:59 +0900

    [ADD] UIFont Extension
    - Custom Font에 대한 타입 프로퍼티명은 figma 디자인 가이드에 맞춤

---
+++ PLUB/Configuration/Extensions/Ex+UIFont.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+extension UIFont {
+  private enum Pretendard: String {
+    
+    // black
+    case black = "Pretendard-Black"
+    
+    // bold
+    case extraBold = "Pretendard-ExtraBold"
+    case bold = "Pretendard-Bold"
+    case semiBold = "Pretendard-SemiBold"
+    
+    // medium
+    case medium = "Pretendard-Medium"
+    
+    // regular
+    case regular = "Pretendard-Regular"
+    
+    // light
+    case light = "Pretendard-Light"
+    case extraLight = "Pretendard-ExtraLight"
+    
+    // thin
+    case thin = "Pretendard-Thin"
+  }
+}
+
+
+extension UIFont {
+  static let h1 = UIFont(name: Pretendard.bold.rawValue, size: 32)
+  static let h2 = UIFont(name: Pretendard.bold.rawValue, size: 28)
+  static let h3 = UIFont(name: Pretendard.semiBold.rawValue, size: 24)
+  static let h4 = UIFont(name: Pretendard.semiBold.rawValue, size: 20)
+  static let h5 = UIFont(name: Pretendard.semiBold.rawValue, size: 18)
+  static let subtitle = UIFont(name: Pretendard.semiBold.rawValue, size: 16)
+  static let body1 = UIFont(name: Pretendard.semiBold.rawValue, size: 14)
+  static let body2 = UIFont(name: Pretendard.regular.rawValue, size: 14)
+  static let button = UIFont(name: Pretendard.bold.rawValue, size: 14)
+  static let caption = UIFont(name: Pretendard.regular.rawValue, size: 12)
+  static let overLine = UIFont(name: Pretendard.regular.rawValue, size: 10)
+}
