75573b86e01387d4662dbcc1ef6fecf5ea00bc55
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-10-26 14:09:15 +0900

    [ADD] Naver, Kakao login button
    - 이미지가 없어 background 색상으로 구분을 줌

--- PLUB/Sources/Login/LoginViewController.swift
+++ PLUB/Sources/Login/LoginViewController.swift
@@ -15,6 +15,14 @@ final class LoginViewController: BaseViewController {
     $0.axis = .vertical
   }
   
+  private let naverLoginButton = UIButton(type: .system).then {
+    $0.backgroundColor = .systemGreen
+  }
+  
+  private let kakaoLoginButton = UIButton(type: .system).then {
+    $0.backgroundColor = .systemYellow
+  }
+  
   private let appleLoginButton = UIButton(type: .system).then {
     $0.backgroundColor = .black
   }
@@ -32,7 +40,7 @@ final class LoginViewController: BaseViewController {
     
     self.view.addSubview(stackView)
     
-    [appleLoginButton].forEach {
+    [naverLoginButton, kakaoLoginButton, appleLoginButton].forEach {
       self.stackView.addArrangedSubview($0)
     }
   }
@@ -43,6 +51,14 @@ final class LoginViewController: BaseViewController {
     self.stackView.snp.makeConstraints { make in
       make.leading.trailing.equalToSuperview().inset(40)
       make.bottom.equalTo(self.view.safeAreaLayoutGuide).inset(164)
+    }
+    
+    self.naverLoginButton.snp.makeConstraints { make in
+      make.height.equalTo(44)
+    }
+    
+    self.kakaoLoginButton.snp.makeConstraints { make in
+      make.height.equalTo(44)
     }
     
     self.appleLoginButton.snp.makeConstraints { make in
