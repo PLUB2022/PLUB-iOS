c18535a0a30ca337d084872ce99169d686adbb96
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-14 17:47:12 +0900

    [FEAT] CheckBox initial state
    - 체크박스 타입에 따라 초기 UI 상태 지정
    1. full
       - 체크모양의 회색배경
    2. none
       - 회색 테두리에 빈 공간

--- PLUB/Configuration/CommonClass/CheckBoxButton.swift
+++ PLUB/Configuration/CommonClass/CheckBoxButton.swift
@@ -17,12 +17,25 @@ final class CheckBoxButton: UIButton {
   init(type: ButtonType) {
     self.checkStyle = type
     super.init(frame: .zero)
+    setupStyles()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
+  private func setupStyles() {
+    self.layer.cornerRadius = 3
+    switch checkStyle {
+    case .full:
+      self.setImage(Image.checkShape, for: .normal)
+      self.backgroundColor = .lightGray
+    case .none:
+      self.backgroundColor = .white
+      self.layer.borderWidth = 2
+      self.layer.borderColor = UIColor.lightGray.cgColor
+    }
+  }
 }
 
 extension CheckBoxButton {
@@ -33,5 +46,11 @@ extension CheckBoxButton {
     
     /// 체크되어있지 않을 때 회색의 체크표시 모양이 보입니다.
     case full
+  }
+}
+
+extension CheckBoxButton {
+  private enum Image {
+    static let checkShape = UIImage(named: "Check")
   }
 }
