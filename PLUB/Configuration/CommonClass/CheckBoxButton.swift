509e7b21a2820287d87621d17e25598179cd809a
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-11-14 17:38:14 +0900

    [ADD] CheckboxButton

---
+++ PLUB/Configuration/CommonClass/CheckBoxButton.swift
@@ -0,0 +1,16 @@
+//
+//  CheckBoxButton.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2022/11/14.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+final class CheckBoxButton: UIButton {
+  
+  
+}
