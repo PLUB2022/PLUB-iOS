ac9ca7c25dad32dd8e88e4a7473e5745aa68bcf4
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-10 22:37:32 +0900

    [ADD] properties in policy header cell
    1. disclosureIndicator: 화살표 이미지 뷰, expandable UX를 위한 구성
    2. policyLabel: 약관 제목
    3. checkbox: 체크박스

--- PLUB/Sources/Login/Policy/PolicyHeaderTableViewCell.swift
+++ PLUB/Sources/Login/Policy/PolicyHeaderTableViewCell.swift
@@ -10,6 +10,25 @@ import UIKit
 final class PolicyHeaderTableViewCell: UITableViewCell {
   
   static let identifier = "\(PolicyHeaderTableViewCell.self)"
+  
+  private let disclosureIndicator: UIImageView = UIImageView().then {
+    $0.image = UIImage(systemName: "chevron.down")
+    $0.contentMode = .scaleAspectFit
+    $0.tintColor = .deepGray
+  }
+  
+  private let policyLabel: UILabel = UILabel().then {
+    $0.numberOfLines = 0
+    $0.font = .body2
+    $0.textColor = .deepGray
+  }
+  
+  private let checkbox: CheckBoxButton = CheckBoxButton(type: .full)
+  
+  private let stackView: UIStackView = UIStackView().then {
+    $0.spacing = 2
+    $0.alignment = .center
+  }
 
   override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
     super.init(style: style, reuseIdentifier: reuseIdentifier)
