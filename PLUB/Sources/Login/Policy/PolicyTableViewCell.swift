7a4578f977fd8c33503838809a73b700762b9cf6
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-10 23:34:29 +0900

    [FEAT] Policy WebView Cell

---
+++ PLUB/Sources/Login/Policy/PolicyTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  PolicyTableViewCell.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2022/12/10.
+//
+
+import WebKit
+import UIKit
+
+import SnapKit
+
+final class PolicyTableViewCell: UITableViewCell {
+  
+  private let webView: WKWebView = WKWebView()
+
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupLayouts()
+    setupConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  private func setupLayouts() {
+    contentView.addSubview(webView)
+  }
+  
+  private func setupConstraints() {
+    webView.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+      make.height.equalTo(208)
+    }
+  }
+  
+  func configure(with url: URL) {
+    let request = URLRequest(url: url)
+    webView.load(request)
+  }
+}
