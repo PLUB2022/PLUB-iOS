a02bc2f901e6c69b8116b552c395c559a0eeda3e
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-18 16:51:10 +0900

    [ADD] Diffable models and types in policy viewmodel

--- PLUB/Sources/Login/Policy/PolicyViewModel.swift
+++ PLUB/Sources/Login/Policy/PolicyViewModel.swift
@@ -5,10 +5,52 @@
 //  Created by 홍승현 on 2022/12/13.
 //
 
-import Foundation
+import UIKit
 
 final class PolicyViewModel {
   
+}
+
+// MARK: - Diffable Models & Types
+
+extension PolicyViewModel {
   
+  typealias DataSource = UITableViewDiffableDataSource<Section, Item>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
   
+  enum Section: Int, CaseIterable {
+    /// 이용약관 및 개인정보 취급 방침
+    case termsOfService
+    /// 위치 기반 서비스 이용 약관
+    case locationBasedService
+    /// 만 14세 이상 확인
+    case termsAndConditionsForAges
+    /// 개인정보 수집 이용 동의
+    case privacyPolicy
+    /// 마케팅 활용 동의
+    case marketing
+  }
+  
+  enum CellType {
+    case header
+    case body
+  }
+  
+  struct Item: Hashable {
+    let type: CellType
+    let url: URL?
+    let policy: String?
+    
+    init(type: CellType, url: URL? = nil, policy: String? = nil) {
+      self.type = type
+      self.url = url
+      self.policy = policy
+    }
+    
+    func hash(into hasher: inout Hasher) {
+      hasher.combine(identifier)
+    }
+    
+    private let identifier = UUID()
+  }
 }
