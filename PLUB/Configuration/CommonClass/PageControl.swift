f6ed5b8e9d33c8a117351fc7fce11c875423978f
SeungHyeon Hong <whi7ehyun@gmail.com>
2022-12-24 23:46:11 +0900

    [ADD] PageControl

---
+++ PLUB/Configuration/CommonClass/PageControl.swift
@@ -0,0 +1,24 @@
+//
+//  PageControl.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2022/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PageControl: UIControl {
+  
+  // MARK: - Initialization
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
