6c16333be3661870f82e50282ca0d96cb00a1f1e
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-04 14:11:42 +0900

    [FEAT] Placeholder in TextView
    TextView에서는 placeholder를 지원하지 않기 때문에
    text를 이용한 눈속임으로 Placeholder처럼 보이도록 구현해두었습니다.
    이를 위해 delegate를 채택하여 알맞은 조건에 placeholder처럼 보이도록 구현해야했습니다.

--- PLUB/Sources/Views/Login/Introduction/IntroductionViewController.swift
+++ PLUB/Sources/Views/Login/Introduction/IntroductionViewController.swift
@@ -29,6 +29,7 @@ final class IntroductionViewController: BaseViewController {
     $0.font = .body2
     $0.backgroundColor = .white
     $0.textContainerInset = UIEdgeInsets(top: 14, left: 8, bottom: 14, right: 8)
+    $0.delegate = self
   }
   
   // MARK: - Configuration
@@ -65,6 +66,28 @@ extension IntroductionViewController {
     return "소개하는 내용을 입력해주세요"
   }
 }
+
+// MARK: - UITextViewDelegate
+
+extension IntroductionViewController: UITextViewDelegate {
+  func textViewDidEndEditing(_ textView: UITextView) {
+    // text가 비어있으면서 글자색이 검정인 경우 -> 사용자가 입력 후 다 지운 경우
+    if textView.text.isEmpty && textView.textColor == .black {
+      textView.text = placeHolder
+      textView.textColor = .deepGray
+    }
+  }
+  
+  func textViewDidBeginEditing(_ textView: UITextView) {
+    // text가 지정된 placeholder면서 글자색이 짙은회색인 경우
+    // 즉 placeholder을 띄운 상태인데 사용자가 수정하려고 textview를 누른 경우
+    if textView.text == placeHolder && textView.textColor == .deepGray {
+      textView.text = ""
+      textView.textColor = .black
+    }
+  }
+}
+
 
 #if canImport(SwiftUI) && DEBUG
 import SwiftUI
