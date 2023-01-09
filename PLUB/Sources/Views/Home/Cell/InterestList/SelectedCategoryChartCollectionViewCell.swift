4317be4e64fb9234ff54319ad27913f52fb887ea
dlrjswns <leekyunjun@gmail.com>
2023-01-10 04:24:43 +0900

    [FIX] Cherrypick에 의한 Merge Conflict 해결

--- PLUB/Sources/Views/Home/Cell/InterestList/SelectedCategoryChartCollectionViewCell.swift
+++ PLUB/Sources/Views/Home/Cell/InterestList/SelectedCategoryChartCollectionViewCell.swift
@@ -10,51 +10,63 @@ import SnapKit
 import Then
 
 struct SelectedCategoryCollectionViewCellModel { // 차트, 그리드일때 둘 다 동일한 모델
-    let title: String
-    let description: String
-    let selectedCategoryInfoViewModel: SelectedCategoryInfoViewModel
+  let title: String
+  let description: String
+  let selectedCategoryInfoViewModel: CategoryInfoListViewModel
 }
 
 class SelectedCategoryChartCollectionViewCell: UICollectionViewCell {
-    static let identifier = "SelectedCategoryChartCollectionViewCell"
-    
-//    public var model: SelectedCategoryCollectionViewCellModel? {
-//        didSet {
-//    }
-    
-    private let titleLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 25, weight: .bold)
-        $0.numberOfLines = 0
-        $0.lineBreakMode = .byTruncatingTail
-        $0.textColor = .white
-        $0.textAlignment = .left
-    }
-    
-    private let descriptionLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 15, weight: .regular)
-        $0.numberOfLines = 1
-        $0.lineBreakMode = .byTruncatingTail
-        $0.textColor = .white
-        $0.textAlignment = .left
-    }
-    
-    private let seperatorView = UIView()
-    
-    private let selectedCategoryInfoView = SelectedCategoryInfoView(selectedCategoryInfoViewType: .Horizontal)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        contentView.layer.cornerRadius = 10
-        contentView.layer.masksToBounds = true
+  static let identifier = "SelectedCategoryChartCollectionViewCell"
+  
+  private let titleLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 25, weight: .bold)
+    $0.numberOfLines = 0
+    $0.lineBreakMode = .byTruncatingTail
+    $0.textColor = .white
+    $0.textAlignment = .left
+  }
+  
+  private let descriptionLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 15, weight: .regular)
+    $0.numberOfLines = 1
+    $0.lineBreakMode = .byTruncatingTail
+    $0.textColor = .white
+    $0.textAlignment = .left
+  }
+  
+  private let seperatorView = UIView()
+  
+  private let selectedCategoryInfoView = CategoryInfoListView(categoryInfoListViewType: .horizontal)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    contentView.layer.cornerRadius = 10
+    contentView.layer.masksToBounds = true
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    titleLabel.text = nil
+    descriptionLabel.text = nil
+    seperatorView.backgroundColor = nil
+    selectedCategoryInfoView.backgroundColor = nil
+  }
+  
+  private func configureUI() {
+    contentView.backgroundColor = .black
+    _ = [titleLabel, descriptionLabel, seperatorView, selectedCategoryInfoView].map{ contentView.addSubview($0) }
+    titleLabel.snp.makeConstraints {
+      $0.top.left.equalToSuperview().offset(20)
+      $0.right.equalToSuperview().offset(-20)
     }
     
     override func prepareForReuse() {
--- PLUB/Sources/Views/Home/Cell/InterestList/SelectedCategoryGridCollectionViewCell.swift
+++ PLUB/Sources/Views/Home/Cell/InterestList/SelectedCategoryGridCollectionViewCell.swift
@@ -8,50 +8,64 @@
 import UIKit
 
 class SelectedCategoryGridCollectionViewCell: UICollectionViewCell {
-    
-    static let identifier = "SelectedCategoryGridCollectionViewCell"
-    
-    private let titleLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 25, weight: .bold)
-        $0.numberOfLines = 1
-        $0.lineBreakMode = .byTruncatingTail
-        $0.textColor = .white
-        $0.textAlignment = .left
+  
+  static let identifier = "SelectedCategoryGridCollectionViewCell"
+  
+  private let titleLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 25, weight: .bold)
+    $0.numberOfLines = 1
+    $0.lineBreakMode = .byTruncatingTail
+    $0.textColor = .white
+    $0.textAlignment = .left
+  }
+  
+  private let descriptionLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 15, weight: .regular)
+    $0.numberOfLines = 1
+    $0.lineBreakMode = .byTruncatingTail
+    $0.textColor = .white
+    $0.textAlignment = .left
+  }
+  
+  private let seperatorView = UIView()
+  
+  private let categoryInfoListView = CategoryInfoListView(categoryInfoListViewType: .vertical)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    contentView.layer.cornerRadius = 10
+    contentView.layer.masksToBounds = true
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    titleLabel.text = nil
+    descriptionLabel.text = nil
+    seperatorView.backgroundColor = nil
+    categoryInfoListView.backgroundColor = nil
+  }
+  
+  private func configureUI() {
+    contentView.backgroundColor = .black
+    _ = [titleLabel, descriptionLabel, seperatorView, categoryInfoListView].map{ contentView.addSubview($0) }
+    categoryInfoListView.snp.makeConstraints {
+      $0.left.equalToSuperview().offset(10)
+      $0.right.bottom.equalToSuperview().offset(-10)
+      $0.height.equalTo(100)
     }
     
-    private let descriptionLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 15, weight: .regular)
-        $0.numberOfLines = 1
-        $0.lineBreakMode = .byTruncatingTail
-        $0.textColor = .white
-        $0.textAlignment = .left
-    }
-    
-    private let seperatorView = UIView()
-    
-    private let selectedCategoryInfoView = SelectedCategoryInfoView(selectedCategoryInfoViewType: .Vertical)
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        configureUI()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        contentView.layer.cornerRadius = 10
-        contentView.layer.masksToBounds = true
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        titleLabel.text = nil
-        descriptionLabel.text = nil
-        seperatorView.backgroundColor = nil
-        selectedCategoryInfoView.backgroundColor = nil
+    seperatorView.snp.makeConstraints {
+      $0.left.right.equalTo(categoryInfoListView)
+      $0.bottom.equalTo(categoryInfoListView.snp.top).offset(-10)
     }
     
     private func configureUI() {
@@ -85,4 +99,12 @@ class SelectedCategoryGridCollectionViewCell: UICollectionViewCell {
         seperatorView.backgroundColor = .white
         selectedCategoryInfoView.configureUI(with: model.selectedCategoryInfoViewModel)
     }
+  }
+  
+  public func configureUI(with model: SelectedCategoryCollectionViewCellModel) {
+    titleLabel.text = model.title
+    descriptionLabel.text = model.description
+    seperatorView.backgroundColor = .white
+    categoryInfoListView.configureUI(with: model.selectedCategoryInfoViewModel)
+  }
 }
