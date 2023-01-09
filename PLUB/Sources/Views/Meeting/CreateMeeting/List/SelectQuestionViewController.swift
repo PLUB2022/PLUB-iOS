0f8d018af0b10744b46a0b37cd42cae1b3642945
soobin-k <tnqls27qks@naver.com>
2023-01-10 01:24:45 +0900

    [ADD] CreateMeetingListViewController의 서브 뷰 컨트롤러 추가, 페이지뷰 간단 구현

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/List/SelectQuestionViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SelectQuestionViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/09.
+//
+
+import UIKit
+
+final class SelectQuestionViewController: BaseViewController {
+  
+  private var superView = UIViewController()
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+    view.backgroundColor = .green
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+  
+  func setupSuperView(superView: UIViewController) {
+      self.superView = superView
+  }
+}
---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/List/SelectTimeViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SelectTimeViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/09.
+//
+
+import UIKit
+
+final class SelectTimeViewController: BaseViewController {
+  
+  private var superView = UIViewController()
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+    view.backgroundColor = .yellow
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+  
+  func setupSuperView(superView: UIViewController) {
+      self.superView = superView
+  }
+}
