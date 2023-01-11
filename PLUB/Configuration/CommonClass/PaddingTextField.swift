e1f611ea784e905fb3e9c296bf6e3691a1d3133b
홍승현 <whi7ehyun@gmail.com>
2023-01-11 14:39:28 +0900

    [로그인] 프로필 사진 & 닉네임 등록 페이지 UI (#29)
    * [ADD] ProfileVC
    
    * [ADD] Profile label in ProfileVC
    
    * [ADD] User's default image assets
    
    * [ADD] Upload image button in ProfileVC
    
    * [ADD] view hirerchy and constraints with stack view in ProfileVC
    
    * [ADD] nickname label in ProfileVC
    
    * [ADD] Nickname TextField in ProfileVC
    
    * [ADD] Alert properties in ProfileVC
    
    * [ADD] StackViews
    
    * [CHORE] Alert label's font
    
    * [FEAT] View hierarchies and constraints
    
    * [ADD] Image assets
    
    * [CHORE] Alert stack view's attributes
    
    * [ADD] alertImageView's image
    
    * [CHORE] alertLabel's text color
    
    * [CHORE] Textfield's layer style
    
    * [ADD] Custom TextField: `IDCheckTextField`
    
    UITextField 내부의 subview의 horizontal padding을 8만큼 설정해야하기 때문에
    커스텀 클래스를 생성하여 만들었습니다. 또한, 나중에 있을 비즈니스 로직을 위해서도
    클래스를 따로 만드는 게 좋을 거라 생각했습니다.
    
    * [ADD] Left(right) view nickname text field
    
    textField 내부의 패딩을 주기 위해 left view와 right view를 추가하였습니다.
    알고있어야하는 점은, left view는 일반 UIView지만, right view는 UIButton이라는 점입니다.
    textfield에 이미 clear버튼이 존재하지만, 항상 보여지게 할 수 없으므로 UIButton을 추가했습니다.
    그래서 해당 UIButton을 누르면 text가 전부 삭제되도록 구현해두었습니다.
    
    * [ADD] Placeholder's attributes in nickname textfield
    
    textField의 placeholder폰트는 body2여야하고
    사용자가 입력하는 텍스트 폰트는 body1이어야합니다.
    그래서 textField의 font 설정으로 전역 폰트설정을 하는 것이 아닌,
    attributedPlaceholder에 접근하여 placeholder만의 폰트를 설정하도록 구현했습니다.
    
    * [ADD] Nickname text field's text binding
    
    text가 입력될 때마다 폰트를 주기적으로 body1으로 설정합니다.
    placeholder의 폰트가 body2라는 점에서 서로 대조된다는 것을 인지해주시면 좋겠습니다.
    
    * [ADD] `Mark` Comments
    
    * [RENAME] IDCheckTextField to `PaddingTextField`
    
    * [ADD] Padding Properties in PaddingTextField
    
    * [ADD] `didSet` that calls viewRect when padding changes
    
    * [ADD] `@discardableResult` to avoid warnings
    
    * [CHORE] nicknameTextField's padding values
    
    * [MOVE] PaddingTextField
    
    다른 곳에서도 사용되는 textField이기에 CommonClass 폴더로 이동시켰습니다.
    
    * [ADD] Comments

---
+++ PLUB/Configuration/CommonClass/PaddingTextField.swift
@@ -0,0 +1,43 @@
+//
+//  PaddingTextField.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/01/04.
+//
+
+import UIKit
+
+final class PaddingTextField: UITextField {
+  
+  // MARK: - Property
+  
+  /// leftView의 left padding을 설정합니다.
+  /// 양수 값이 클 수록 왼쪽으로부터 멀리 떨어지며, 음수 값을 줄 경우, 왼쪽 라인을 넘어가 parent view를 벗어납니다.
+  var leftViewPadding: CGFloat = 0 {
+    didSet {
+      self.leftViewRect(forBounds: bounds)
+    }
+  }
+  
+  /// rightView의 right padding을 설정합니다.
+  /// 양수 값이 클 수록 오른쪽으로부터 멀리 떨어지며, 음수 값을 줄 경우, 오른쪽 라인을 넘어가므로 parent view를 벗어납니다.
+  var rightViewPadding: CGFloat = 0 {
+    didSet {
+      self.rightViewRect(forBounds: bounds)
+    }
+  }
+  
+  @discardableResult
+  override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
+    var leftViewRect = super.leftViewRect(forBounds: bounds)
+    leftViewRect.origin.x += leftViewPadding
+    return leftViewRect
+  }
+  
+  @discardableResult
+  override func rightViewRect(forBounds bounds: CGRect) -> CGRect {
+    var rightViewRect = super.rightViewRect(forBounds: bounds)
+    rightViewRect.origin.x -= rightViewPadding
+    return rightViewRect
+  }
+}
