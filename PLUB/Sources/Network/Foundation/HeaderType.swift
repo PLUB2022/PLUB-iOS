db627ac6972139f26b4c433c4241b7db3497b6e5
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-13 16:12:39 +0900

    [ADD] HeaderType's toHTTPHeader property
    request 객체에 header를 주입하기 위해 각 case에 맞는 연산 프로퍼티를 추가했습니다.
    withToken case는 아직 미완성입니다. 에러 방지를 위해 비슷한 플로우의 테스트코드를 넣었습니다.

--- PLUB/Sources/Network/Foundation/HeaderType.swift
+++ PLUB/Sources/Network/Foundation/HeaderType.swift
@@ -9,8 +9,29 @@ import Foundation
 
 import Alamofire
 
-
 enum HeaderType {
   case `default`
   case withToken
+}
+
+extension HeaderType {
+  var toHTTPHeader: HTTPHeaders {
+    switch self {
+    case .default:
+      return .default
+    //TODO: 승현 - 아직 테스트 코드입니다. UserManager 구현 후 코드가 대체될 예정입니다.
+    case .withToken:
+      @KeyChainWrapper(key: "accessToken")
+      var token: String?
+      
+      // 토큰이 존재하지 않는 경우
+      guard let token = token else { return .default }
+      
+      // default 헤더 값에 `Authorization token` 및 `Content-Type` 추가
+      var defaultHeaders = HTTPHeaders.default
+      defaultHeaders.add(.authorization(bearerToken: token))
+      defaultHeaders.add(.contentType("application/json"))
+      return defaultHeaders
+    }
+  }
 }
