80eb3957697f25739e3545362e2d4911ddd24af0
soobin-k <tnqls27qks@naver.com>
2023-01-15 01:51:36 +0900

    [ADD] MeetingIntroduceViewController 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/List/MeetingIntroduceViewController.swift
@@ -0,0 +1,93 @@
+//
+//  MeetingIntroduceViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/15.
+//
+
+import UIKit
+
+import RxSwift
+import SnapKit
+
+final class MeetingIntroduceViewController: BaseViewController {
+  // MARK: - Property
+  private let scrollView = UIScrollView().then {
+    $0.bounces = false
+    $0.contentInsetAdjustmentBehavior = .never
+    $0.showsVerticalScrollIndicator = false
+  }
+  
+  private let contentStackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 43
+  }
+  
+  private let titleView = CreateMeetingTitleView(
+    title: "우리 모임은 어떤 모임인가요?",
+    description: "모임에 대해서 자세하게 설명해주세요."
+  )
+    
+  private let goalView = InputTextView(
+    title: "모임 목표",
+    placeHolder: "소개하는 내용을 입력해주세요",
+    options: [.textCount]
+  )
+  
+  private let introduceView = InputTextView(
+    title: "모임 소개글",
+    placeHolder: "우리동네 사진모임"
+  )
+  
+  private let photoStackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 8
+  }
+  
+  private let photoTitleLabel = UILabel().then {
+    $0.font = .subtitle
+    $0.textColor = .black
+    $0.text = "모임 소개 사진 (선택)"
+  }
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    view.addSubview(scrollView)
+    scrollView.addSubview(contentStackView)
+    contentStackView.addArrangedSubview(titleView)
+    contentStackView.addArrangedSubview(goalView)
+    contentStackView.addArrangedSubview(introduceView)
+    contentStackView.addArrangedSubview(photoStackView)
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    scrollView.snp.makeConstraints {
+        $0.top.equalTo(view.safeAreaLayoutGuide)
+        $0.leading.trailing.bottom.equalToSuperview()
+    }
+    
+    contentStackView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+      $0.width.equalTo(scrollView.snp.width)
+    }
+    
+    contentStackView.setCustomSpacing(48, after: titleView)
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+}
