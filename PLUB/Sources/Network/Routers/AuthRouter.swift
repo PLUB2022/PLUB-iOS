0e7e22423cd485689bd3f1d96eeaf7af4458dce6
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-16 01:55:02 +0900

    [FEAT] AuthRouter에서 Router을 준수

--- PLUB/Sources/Network/Routers/AuthRouter.swift
+++ PLUB/Sources/Network/Routers/AuthRouter.swift
@@ -10,3 +10,34 @@ import Alamofire
 enum AuthRouter {
   case socialLogin(type: SignInType, token: String?, authorizationCode: String?)
 }
+
+extension AuthRouter: Router {
+  
+  var method: HTTPMethod {
+    switch self {
+    case .socialLogin:
+      return .post
+    }
+  }
+  
+  var path: String {
+    switch self {
+    case .socialLogin:
+      return "/auth/login"
+    }
+  }
+  
+  var parameters: ParameterType {
+    switch self {
+    case let .socialLogin(socialType, accessToken, authorizationCode):
+      return .body(SignInRequest(accessToken: accessToken, authorizationCode: authorizationCode, socialType: socialType))
+    }
+  }
+  
+  var headers: HeaderType {
+    switch self {
+    case .socialLogin:
+      return .default
+    }
+  }
+}
