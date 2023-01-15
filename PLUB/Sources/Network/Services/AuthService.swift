e95e0647ff7563e5e714cd752582d60b1df8356a
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-16 01:56:42 +0900

    [ADD] AuthService

---
+++ PLUB/Sources/Network/Services/AuthService.swift
@@ -0,0 +1,14 @@
+//
+//  AuthService.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/01/16.
+//
+
+import Foundation
+
+class AuthService: BaseService {
+  static let shared = AuthService()
+  
+  private override init() { }
+}
