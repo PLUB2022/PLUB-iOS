7f45b801c998b653b12fdffcca055c2db877e23a
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-16 02:09:12 +0900

    [FEAT] AuthService's requestAuth(_:_:_:)
    OAuth로 소셜로그인 요청을 보내는 메서드 구현

--- PLUB/Sources/Network/Services/AuthService.swift
+++ PLUB/Sources/Network/Services/AuthService.swift
@@ -7,8 +7,24 @@
 
 import Foundation
 
+import RxCocoa
+import RxSwift
+
 class AuthService: BaseService {
   static let shared = AuthService()
   
   private override init() { }
+}
+
+extension AuthService {
+  func requestAuth(
+    socialType: SignInType,
+    token: String?,
+    authorizationCode: String?
+  ) -> Observable<Result<GeneralResponse<SignInResponse>, PLUBError>> {
+    return sendRequest(
+      AuthRouter.socialLogin(type: socialType, token: token, authorizationCode: authorizationCode),
+      type: SignInResponse.self
+    )
+  }
 }
