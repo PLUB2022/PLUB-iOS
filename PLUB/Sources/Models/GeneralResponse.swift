0a1397b0f41a59df498c9feac04e76120b45bbb4
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-15 21:20:25 +0900

    [ADD] Custom Decoder Initializations in GeneralResponse

--- PLUB/Sources/Models/GeneralResponse.swift
+++ PLUB/Sources/Models/GeneralResponse.swift
@@ -11,4 +11,17 @@ struct GeneralResponse<T>: Decodable where T: Decodable {
   let statusCode: Int?
   let message: String?
   let data: T?
+  
+  enum CodingKeys: CodingKey {
+    case statusCode
+    case message
+    case data
+  }
+  
+  init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    self.statusCode = try? container.decodeIfPresent(Int.self, forKey: .statusCode)
+    self.message = try? container.decodeIfPresent(String.self, forKey: .message)
+    self.data = try? container.decodeIfPresent(T.self, forKey: .data)
+  }
 }
