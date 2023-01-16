3c9f0c2eb3a5c760c1282470b0ead24063da6150
dlrjswns <leekyunjun@gmail.com>
2023-01-16 22:32:12 +0900

    [FIX] 특정 경우에 원치않은 이슈로 인한 코드 수정

--- PLUB/Sources/Views/Home/ViewModel/ApplyQuestionViewModel.swift
+++ PLUB/Sources/Views/Home/ViewModel/ApplyQuestionViewModel.swift
@@ -29,7 +29,6 @@ class ApplyQuestionViewModel: ApplyQuestionViewModelType {
   
   init() {
     let questions = BehaviorSubject<[ApplyQuestionTableViewCellModel]>(value: [])
-    let writingCount = BehaviorSubject<Int>(value: 0)
     let currentQuestion = PublishSubject<QuestionStatus>()
     let isActivating = BehaviorSubject<Bool>(value: false)
     let entireQuestionStatus = PublishSubject<[QuestionStatus]>()
@@ -51,87 +50,22 @@ class ApplyQuestionViewModel: ApplyQuestionViewModelType {
       .subscribe(onNext: { (currentStatus, entireStatus) in
         let entireStatus = entireStatus.map { status -> QuestionStatus in
           if status.id == currentStatus.id {
-            return QuestionStatus(id: status.id, isFilled: currentStatus.isFilled)
-          } else {
             return QuestionStatus(id: currentStatus.id, isFilled: currentStatus.isFilled)
+          } else {
+            return QuestionStatus(id: status.id, isFilled: status.isFilled)
           }
         }
-        print("ststs = \(entireStatus)")
         entireQuestionStatus.onNext(entireStatus)
       })
       .disposed(by: disposeBag)
     
-    let entireQuestionCount = questions.map { $0.count }
-    
-    Observable.combineLatest(writingCount, entireQuestionStatus) { ($0, $1) }
-      .map { (questionStatus, entireQuestionStatus) in
-            
-      }
-    
-    entireQuestionStatus.subscribe(onNext: { entire in
-      print("entire = \(entire)")
-    })
-    .disposed(by: disposeBag)
-//    let checkStatus = currentQuestion.withLatestFrom(entireQuestionStatus) { ($0, $1) }
-//      .map { (currentStatus, entireStatus) in
-//        entireStatus.filter { status in
-//          return currentStatus == status
-//        }
-//      }
-      
-    
-    let plusCount = entireQuestionStatus.distinctUntilChanged()
-      .withLatestFrom(writingCount) { ($0, $1) }
-      .filter({ (entireStatus, count) in
-        print("오잉 = \(entireStatus)")
-        for status in entireStatus {
-          if status.isFilled {
-            return true
-          }
-        }
-        return false
-      })
-      .map { $0.1 + 1 }
-    
-    let checkAllTrue = entireQuestionStatus.filter { entireStatus in
-      if entireStatus.contains(where: { !$0.isFilled }) {
-        return false
-      }
-      return true
-    }
-    
-    checkAllTrue.map { $0.count != 0 }
-      .bind(to: isActivating)
-      .disposed(by: disposeBag)
-    
-//    let plusCount = currentQuestion.distinctUntilChanged()
-//      .withLatestFrom(writingCount) { ($0, $1) }
-//      .filter { $0.0.isFilled }
-//      .map { $0.1 + 1 }
-    
-//    let minusCount = currentQuestion.distinctUntilChanged()
-//      .withLatestFrom(writingCount) { ($0, $1) }
-//      .filter { !$0.0.isFilled }
-//      .map { $0.1 - 1 }
-    
-//    Observable.merge(plusCount, minusCount)
-//      .bind(to: writingCount)
-//      .disposed(by: disposeBag)
-//
-//    Observable.combineLatest(writingCount, entireQuestionCount) { ($0, $1) }
-//      .map { $0.0 == $0.1 }
-//      .bind(to: isActivating)
-//      .disposed(by: disposeBag)
-    
-    currentQuestion.distinctUntilChanged().subscribe(onNext: { current in
-      print("current = \(current)")
-    })
-    .disposed(by: disposeBag)
-    
-    writingCount.subscribe(onNext: { count in
-      print("count = \(count)")
-    })
-    .disposed(by: disposeBag)
+    entireQuestionStatus
+          .distinctUntilChanged()
+          .subscribe(onNext: { current in
+            let isAllChecked = current.filter{ $0.isFilled == false }.count == 0
+            isActivating.onNext(isAllChecked)
+          })
+          .disposed(by: disposeBag)
     
     questions.onNext([
       .init(id: 1,question: "1. 지원 동기가 궁금해요!"),
