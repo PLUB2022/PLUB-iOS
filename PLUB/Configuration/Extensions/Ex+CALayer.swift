a408f6ca4927fa3e6513978d18580b0ca6870754
soobin-k <tnqls27qks@naver.com>
2023-01-17 01:16:50 +0900

    [CHORE] applyShadow spread 항목 추가

--- PLUB/Configuration/Extensions/Ex+CALayer.swift
+++ PLUB/Configuration/Extensions/Ex+CALayer.swift
@@ -13,11 +13,18 @@ extension CALayer {
     alpha: Float = 0.5,
     x: CGFloat = 0,
     y: CGFloat = 2,
-    blur: CGFloat = 4
+    blur: CGFloat = 4,
+    spread: CGFloat = 0
   ) {
     shadowColor = color.cgColor
     shadowOpacity = alpha
     shadowOffset = CGSize(width: x, height: y)
     shadowRadius = blur / 2.0
+    
+    guard spread != 0 else { shadowPath = nil; return }
+    
+    let dxValue = -spread
+    let rect = bounds.insetBy(dx: dxValue, dy: dxValue)
+    shadowPath = UIBezierPath(rect: rect).cgPath
   }
 }
