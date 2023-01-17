b5c816849c3a1b04962417bd1133884317044bff
soobin-k <tnqls27qks@naver.com>
2023-01-17 23:31:13 +0900

    [ADD] PhotoBottomSheetViewController 추가 - BottomSheetViewController 상속해 구현

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/Component/PhotoBottomSheetViewController.swift
@@ -0,0 +1,76 @@
+//
+//  PhotoBottomSheetViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/17.
+//
+
+import Foundation
+import UIKit
+
+final class PhotoBottomSheetViewController: BottomSheetViewController {
+  private let lineView = UIView().then {
+    $0.backgroundColor = .mediumGray
+    $0.layer.cornerRadius = 6
+  }
+  
+  private let contentStackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 8
+    $0.backgroundColor = .background
+  }
+  
+  private let cameraView = PhotoBottomSheetListView(
+    text: "카메라로 촬영",
+    image: "camera"
+  )
+  
+  private let albumView = PhotoBottomSheetListView(
+    text: "앨범에서 사진 업로드",
+    image: "selectPhotoBlack"
+  )
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    [lineView, contentStackView].forEach {
+      contentView.addSubview($0)
+    }
+    
+    [cameraView, albumView].forEach {
+      contentStackView.addArrangedSubview($0)
+    }
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    
+    lineView.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(8)
+      $0.height.equalTo(4)
+      $0.width.equalTo(48)
+      $0.centerX.equalToSuperview()
+    }
+    
+    contentStackView.snp.makeConstraints {
+      $0.top.equalTo(lineView.snp.bottom).offset(26)
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.bottom.equalToSuperview().inset(78)
+    }
+    
+    cameraView.snp.makeConstraints {
+      $0.height.equalTo(52)
+    }
+    
+    albumView.snp.makeConstraints {
+      $0.height.equalTo(52)
+    }
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+}
