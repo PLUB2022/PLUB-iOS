1082b75b374c1bde4dff168123b98587cf259c12
soobin-k <tnqls27qks@naver.com>
2023-01-18 22:50:48 +0900

    [CHORE] pr 리뷰 반영

--- PLUB/Configuration/CommonClass/BottomSheetViewController.swift
+++ PLUB/Configuration/CommonClass/BottomSheetViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 import RxSwift
 
 class BottomSheetViewController: BaseViewController {
+  let backgroundView = UIView().then {
+    $0.backgroundColor = .init(hex: 0x000000).withAlphaComponent(0.25)
+  }
+  
   let contentView = UIView().then {
     $0.backgroundColor = .background
     $0.layer.cornerRadius = 20
@@ -21,29 +25,28 @@ class BottomSheetViewController: BaseViewController {
       action: nil
   )
   
-  init() {
-    super.init(nibName: nil, bundle: nil)
-    view.backgroundColor = .clear
-  }
-  
-  required init?(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
   override func viewWillAppear(_ animated: Bool) {
     presentBottomSheet()
   }
   
   override func setupLayouts() {
-    [contentView].forEach {
+    [backgroundView, contentView].forEach {
       view.addSubview($0)
     }
   }
   
   override func setupConstraints() {
+    backgroundView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+    
     contentView.snp.makeConstraints {
       $0.leading.trailing.bottom.equalToSuperview()
     }
+  }
+  
+  override func setupStyles() {
+    view.backgroundColor = .clear
   }
   
   override func bind() {
@@ -54,7 +57,7 @@ class BottomSheetViewController: BaseViewController {
         self.dismissBottomSheet()
       })
       .disposed(by: disposeBag)
-    view.addGestureRecognizer(tapGesture)
+    backgroundView.addGestureRecognizer(tapGesture)
   }
 }
 
@@ -62,14 +65,14 @@ private extension BottomSheetViewController {
   private func presentBottomSheet() {
     contentView.frame.origin = CGPoint(x: 0, y: UIScreen.main.bounds.height)
     UIView.transition(with: view, duration: 0.25, options: .curveEaseInOut, animations: {
-      self.view.backgroundColor = .init(hex: 0x000000).withAlphaComponent(0.25)
+      self.backgroundView.backgroundColor = .init(hex: 0x000000).withAlphaComponent(0.25)
       self.contentView.frame.origin = CGPoint(x: 0, y: 0)
     })
   }
   
   private func dismissBottomSheet() {
     UIView.transition(with: view, duration: 0.25, options: .curveEaseInOut, animations: {
-      self.view.backgroundColor = .clear
+      self.backgroundView.backgroundColor = .clear
       self.contentView.frame.origin = CGPoint(x: 0, y: UIScreen.main.bounds.height)
     }, completion: { _ in
       self.dismiss(animated: false)
