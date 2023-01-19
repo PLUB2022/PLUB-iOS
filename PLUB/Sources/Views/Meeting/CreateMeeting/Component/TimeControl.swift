9f7ea4ba1ba16bf28db996b4416113fe89db211f
soobin-k <tnqls27qks@naver.com>
2023-01-20 01:21:18 +0900

    [ADD] TimeControl 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/Component/TimeControl.swift
@@ -0,0 +1,91 @@
+//
+//  TimeControl.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/20.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+final class TimeControl: UIControl {
+
+  var date = Date() {
+    didSet {
+      label.text = DateFormatter().then {
+        $0.dateFormat = "a hh시 mm분"
+        $0.locale = Locale(identifier: "ko_KR")
+      }.string(from: date)
+    }
+  }
+
+  override var isSelected: Bool {
+    didSet {
+      if isSelected {
+        imageView.image = UIImage(named: "clockActivated")
+        setupStyles()
+      }
+    }
+  }
+
+  private let label = UILabel().then {
+    $0.text = "시간을 선택해주세요."
+    $0.font = .subtitle
+  }
+
+  private let imageView = UIImageView().then {
+    $0.image = UIImage(named: "clockInactivated")
+    $0.contentMode = .scaleAspectFit
+  }
+
+  init() {
+    super.init(frame: .zero)
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupLayouts() {
+    addSubview(label)
+    addSubview(imageView)
+  }
+
+  private func setupConstraints() {
+    imageView.snp.makeConstraints {
+      $0.leading.equalToSuperview().inset(8)
+      $0.centerY.equalToSuperview()
+      $0.size.equalTo(32)
+    }
+    
+    label.snp.makeConstraints {
+      $0.leading.equalTo(imageView.snp.trailing).offset(8)
+      $0.centerY.equalTo(imageView.snp.centerY)
+    }
+  }
+
+  private func setupStyles() {
+    layer.borderWidth = 1
+    layer.cornerRadius = 8
+    if isSelected {
+      label.textColor = .black
+      layer.borderColor = UIColor.main.cgColor
+    } else {
+      label.textColor = .deepGray
+      layer.borderColor = UIColor.deepGray.cgColor
+    }
+  }
+}
+
+extension Reactive where Base: TimeControl {
+
+  /// Reactive wrapper for `TouchUpInside` control event.
+  var tap: ControlEvent<Void> {
+    controlEvent(.touchUpInside)
+  }
+}
