c9f5010d13d14cb610b0e45d0b095349fde776a1
dlrjswns <leekyunjun@gmail.com>
2023-01-19 19:20:02 +0900

    [ADD] CategoryService 추가

---
+++ PLUB/Sources/Network/Services/CategoryService.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryService.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/01/19.
+//
+
+import Foundation
+
+class CategoryService: BaseService {
+  static let shared = CategoryService()
+  
+  private override init() { }
+}
+
+extension CategoryService {
+  
+}
