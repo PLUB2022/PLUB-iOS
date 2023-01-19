2d7ddc0e1e687a62070fa8a8dcabacc10ce4b6c9
dlrjswns <leekyunjun@gmail.com>
2023-01-19 19:50:52 +0900

    [FEAT] CategoryRouter 기능 추가

--- PLUB/Sources/Network/Routers/CategoryRouter.swift
+++ PLUB/Sources/Network/Routers/CategoryRouter.swift
@@ -10,9 +10,30 @@ import Alamofire
 enum CategoryRouter {
   case inquireMainCategoryList
   case inquireAll
-  case inquireSubCategoryList
+  case inquireSubCategoryList(Int)
 }
 
 extension CategoryRouter: Router {
+  var method: HTTPMethod {
+    return .get
+  }
   
+  var path: String {
+    switch self {
+    case .inquireMainCategoryList:
+      return "/categories"
+    case .inquireAll:
+      return "/categories/all"
+    case .inquireSubCategoryList(let categoryId):
+      return "/categories/\(categoryId)/sub"
+    }
+  }
+  
+  var parameters: ParameterType {
+    return .plain
+  }
+  
+  var headers: HeaderType {
+    return .default
+  }
 }
