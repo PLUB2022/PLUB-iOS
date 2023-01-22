575be9ab2cd7d3da331cf4a18e342318821dd0f0
dlrjswns <leekyunjun@gmail.com>
2023-01-23 01:07:54 +0900

    [ADD] RecruitmentRouter 파일 추가

---
+++ PLUB/Sources/Network/Routers/RecruitmentRouter.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryRouter.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/01/19.
+//
+
+import Alamofire
+
+enum RecruitmentRouter {
+  
+}
+
+extension RecruitmentRouter: Router {
+  var method: HTTPMethod {
+    
+      return .get
+
+  }
+  
+  var path: String {
+    switch self {
+    
+    }
+  }
+  
+  var parameters: ParameterType {
+    switch self {
+    
+    }
+  }
+  
+  var headers: HeaderType {
+    switch self {
+    
+    }
+  }
+}
+
+
