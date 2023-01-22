44eabb0002c50d797c41c76c2d1311c84425bd42
dlrjswns <leekyunjun@gmail.com>
2023-01-23 02:20:52 +0900

    [FEAT] 소개카테고리화면과 관련된 viewModel 코드 추가

--- PLUB/Sources/Views/Home/ViewModel/IntroduceCategoryViewModel.swift
+++ PLUB/Sources/Views/Home/ViewModel/IntroduceCategoryViewModel.swift
@@ -5,8 +5,48 @@
 //  Created by 이건준 on 2023/01/23.
 //
 
-import Foundation
+import RxSwift
+import RxCocoa
 
-class IntroduceCategoryViewModel {
+protocol IntroduceCategoryViewModelType {
+  // Input
+  var selectPlubbingId: AnyObserver<String> { get }
   
+  // Output
+  var fetchDetailRecruitment: AnyObserver<DetailRecruitmentModel> { get }
+}
+
+class IntroduceCategoryViewModel: IntroduceCategoryViewModelType {
+  
+  private var disposeBag = DisposeBag()
+  
+  // Input
+  let selectPlubbingId: AnyObserver<String>
+  
+  // Output
+  let fetchDetailRecruitment: AnyObserver<DetailRecruitmentModel>
+  
+  init() {
+    let selectingPlubbingId = PublishSubject<String>()
+    let fetchingDetailRecruitment = PublishSubject<DetailRecruitmentModel>()
+    
+    self.selectPlubbingId = selectingPlubbingId.asObserver()
+    self.fetchDetailRecruitment = fetchingDetailRecruitment.asObserver()
+    
+    let fetchingDetail = selectingPlubbingId
+      .flatMapLatest(RecruitmentService.shared.inquireDetailRecruitment(plubbingId:))
+    
+    let successFetchingDetail = fetchingDetail.map { result -> DetailRecruitmentResponse? in
+      guard case .success(let detailRecruitmentResponse) = result else { return nil }
+      return detailRecruitmentResponse.data
+    }
+    
+    successFetchingDetail
+      .subscribe(onNext: { response in
+        guard let response = response else { return }
+        let model = DetailRecruitmentModel(response: response)
+        fetchingDetailRecruitment.onNext(model)
+      })
+      .disposed(by: disposeBag)
+  }
 }
