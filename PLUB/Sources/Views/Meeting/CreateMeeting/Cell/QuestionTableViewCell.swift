d8a0e167f63556d8bed9640a6152bcd6a39a4639
soobin-k <tnqls27qks@naver.com>
2023-01-22 17:10:18 +0900

    [ADD] 질문 셀 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/Cell/QuestionTableViewCell.swift
@@ -0,0 +1,55 @@
+//
+//  QuestionTableViewCell.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class QuestionTableViewCell: UITableViewCell {
+    
+  static let identifier = "QuestionTableViewCell"
+  
+  private let textView = InputTextView(
+    title: "질문",
+    placeHolder: "질문을 입력해주세요"
+  )
+    
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+    
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    textView.textView.text = nil
+  }
+}
+
+private extension QuestionTableViewCell {
+  func setupLayouts() {
+    contentView.addSubview(textView)
+  }
+  
+  func setupConstraints() {
+    textView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+  
+  func setupStyles() {
+    selectionStyle = .none
+    accessoryType = .none
+    backgroundColor = .background
+  }
+}
