ff274fb5136405fabb68bae8d11df8721a0614b5
dlrjswns <leekyunjun@gmail.com>
2023-01-23 01:13:48 +0900

    [FEAT] RecruitmentRouter 모집글상세조회 API 추가

--- PLUB/Sources/Network/Routers/RecruitmentRouter.swift
+++ PLUB/Sources/Network/Routers/RecruitmentRouter.swift
@@ -8,31 +8,35 @@
 import Alamofire
 
 enum RecruitmentRouter {
-  
+  case inquireDetailRecruitment(String)
 }
 
 extension RecruitmentRouter: Router {
   var method: HTTPMethod {
-    
+    switch self {
+    case .inquireDetailRecruitment:
       return .get
-
+    }
   }
   
   var path: String {
     switch self {
-    
+    case .inquireDetailRecruitment(let plubbingId):
+      return "/plubbings/\(plubbingId)/recruit"
     }
   }
   
   var parameters: ParameterType {
     switch self {
-    
+    case .inquireDetailRecruitment:
+      return .plain
     }
   }
   
   var headers: HeaderType {
     switch self {
-    
+    case .inquireDetailRecruitment:
+      return .withAccessToken
     }
   }
 }
