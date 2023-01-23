d1300cc3dbc73016f0a9514baece5681fb8f9f8f
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-23 23:42:03 +0900

    [ADD] InterestViewController

---
+++ PLUB/Sources/Views/Login/Interest/InterestViewController.swift
@@ -0,0 +1,17 @@
+//
+//  InterestViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/01/23.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+import SnapKit
+import Then
+
+final class InterestViewController: BaseViewController {
+  
+}
