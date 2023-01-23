445045cd299c0e3ab054b11793319b5d40c0a724
dlrjswns <leekyunjun@gmail.com>
2023-01-23 21:42:46 +0900

    [FEAT] 모집질문조회API관련 라우터 기능 추가

--- PLUB/Sources/Network/Routers/RecruitmentRouter.swift
+++ PLUB/Sources/Network/Routers/RecruitmentRouter.swift
@@ -9,12 +9,13 @@ import Alamofire
 
 enum RecruitmentRouter {
   case inquireDetailRecruitment(String)
+  case inquireRecruitmentQuestion(String)
 }
 
 extension RecruitmentRouter: Router {
   var method: HTTPMethod {
     switch self {
-    case .inquireDetailRecruitment:
+    case .inquireDetailRecruitment, .inquireRecruitmentQuestion:
       return .get
     }
   }
@@ -23,19 +24,21 @@ extension RecruitmentRouter: Router {
     switch self {
     case .inquireDetailRecruitment(let plubbingId):
       return "/plubbings/\(plubbingId)/recruit"
+    case .inquireRecruitmentQuestion(let plubbingId):
+      return "/plubbings/\(plubbingId)/recruit/questions"
     }
   }
   
   var parameters: ParameterType {
     switch self {
-    case .inquireDetailRecruitment:
+    case .inquireDetailRecruitment, .inquireRecruitmentQuestion:
       return .plain
     }
   }
   
   var headers: HeaderType {
     switch self {
-    case .inquireDetailRecruitment:
+    case .inquireDetailRecruitment, .inquireRecruitmentQuestion:
       return .withAccessToken
     }
   }
