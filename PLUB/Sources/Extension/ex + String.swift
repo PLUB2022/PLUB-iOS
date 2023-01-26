0c6f5abd5fec608966fe5bb473a2528f31e7e665
dlrjswns <leekyunjun@gmail.com>
2023-01-26 23:33:40 +0900

    [FEAT] 서버 Day응답값에 따른 파싱함수 구현

---
+++ PLUB/Sources/Extension/ex + String.swift
@@ -0,0 +1,43 @@
+//
+//  ex + String.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/01/26.
+//
+
+extension String {
+  
+  func fromENGToKOR() -> Self {
+    guard let day = Day.allCases.filter({ $0.toKOR == self }).first else { return "" }
+    return day.toKOR
+  }
+  
+  enum Day: CaseIterable {
+    case MON
+    case TUE
+    case WED
+    case THR
+    case FRI
+    case SAT
+    case SUN
+    
+    var toKOR: String {
+      switch self {
+      case .MON:
+        return "월"
+      case .TUE:
+        return "화"
+      case .WED:
+        return "수"
+      case .THR:
+        return "목"
+      case .FRI:
+        return "금"
+      case .SAT:
+        return "토"
+      case .SUN:
+        return "일"
+      }
+    }
+  }
+}
