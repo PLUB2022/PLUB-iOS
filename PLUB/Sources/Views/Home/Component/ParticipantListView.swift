3d10a5e24c9a741f91c9b1c9d4fc7a155117ceb2
dlrjswns <leekyunjun@gmail.com>
2023-01-26 19:51:51 +0900

    [CHORE] 스택뷰사용을 위한 컬렉션 뷰 코드 삭제 및 참여자프로필이미지컴포넌트 추가

--- PLUB/Sources/Views/Home/Component/ParticipantListView.swift
+++ PLUB/Sources/Views/Home/Component/ParticipantListView.swift
@@ -26,19 +26,12 @@ class ParticipantListView: UIView {
     $0.sizeToFit()
   }
   
-  private lazy var participantListStackView = UIStackView(arrangedSubviews: [
-    profileImageView, moreButton
-  ]).then {
+  private let participantListStackView = UIStackView().then {
     $0.spacing = 8.05
     $0.axis = .horizontal
     $0.alignment = .leading
   }
-  
-  private let profileImageView = UIImageView().then {
-    $0.contentMode = .scaleAspectFit
-    $0.image = UIImage(systemName: "person.fill")
-  }
-  
+
   private let moreButton = UIButton().then {
     $0.backgroundColor = .lightGray
     $0.tintColor = .deepGray
@@ -72,10 +65,6 @@ class ParticipantListView: UIView {
       $0.left.right.bottom.equalToSuperview()
     }
     
-    profileImageView.snp.makeConstraints {
-      $0.width.height.equalTo(34)
-    }
-    
     moreButton.snp.makeConstraints {
       $0.width.height.equalTo(34)
     }
@@ -83,8 +72,44 @@ class ParticipantListView: UIView {
     moreButton.addTarget(self, action: #selector(didTappedMoreButton), for: .touchUpInside)
   }
   
+  public func configureUI(with model: [AccountInfo]) {
+    let joinedCount = model.count
+    guard joinedCount < 1 else { return }
+    if joinedCount <= 8 {
+      
+    } else { // 9명 이상일때
+      
+    }
+  }
+  
   @objc private func didTappedMoreButton() {
     delegate?.didTappedMoreButton()
   }
 }
 
+final class ParticipantImageView: UIImageView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    let imageSize = self.bounds.size.width
+    layer.cornerRadius = imageSize / 2
+  }
+  
+  private func configureUI() {
+    layer.masksToBounds = true
+    contentMode = .scaleAspectFit
+    image = UIImage(systemName: "person.fill")
+    backgroundColor = .yellow
+    snp.makeConstraints { make in
+      make.size.equalTo(34)
+    }
+  }
+}
