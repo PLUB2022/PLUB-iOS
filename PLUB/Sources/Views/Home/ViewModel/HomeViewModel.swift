7941af27c3ae1bab4f3a05b549b0c3c0fc52c54a
dlrjswns <leekyunjun@gmail.com>
2023-01-28 23:23:06 +0900

    [RENAME] successFetching -> successFetchingMainCategoryList로 이름 변경

--- PLUB/Sources/Views/Home/ViewModel/HomeViewModel.swift
+++ PLUB/Sources/Views/Home/ViewModel/HomeViewModel.swift
@@ -28,12 +28,12 @@ class HomeViewModel: HomeViewModelType {
     self.fetchedMainCategoryList = fetchingMainCategoryList.asDriver(onErrorDriveWith: .empty())
     
     let inquireMainCategoryList = CategoryService.shared.inquireMainCategoryList().share()
-    let successFetching = inquireMainCategoryList.map { result -> [MainCategory]? in
+    let successFetchingMainCategoryList = inquireMainCategoryList.map { result -> [MainCategory]? in
       guard case .success(let mainCategoryListResponse) = result else { return nil }
       return mainCategoryListResponse.data?.categories
     }
     
-    successFetching.subscribe(onNext: { mainCategories in
+    successFetchingMainCategoryList.subscribe(onNext: { mainCategories in
       guard let mainCategories = mainCategories else { return }
       fetchingMainCategoryList.onNext(mainCategories)
     })
