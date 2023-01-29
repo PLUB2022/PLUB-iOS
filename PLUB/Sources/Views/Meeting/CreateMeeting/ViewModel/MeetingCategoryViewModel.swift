929284d6ede30f979fb04314ffaf5a173f186ec1
soobin-k <tnqls27qks@naver.com>
2023-01-29 23:41:25 +0900

    [ADD] 카테고리 뷰모델 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/ViewModel/MeetingCategoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  MeetingCategoryViewModel.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/29.
+//
+
+import RxSwift
+import RxCocoa
+
+final class MeetingCategoryViewModel {
+  private let disposeBag = DisposeBag()
+}
