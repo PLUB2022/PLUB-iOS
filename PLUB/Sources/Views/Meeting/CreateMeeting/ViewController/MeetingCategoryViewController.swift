271e592c98d8050368394af55f349c876bc879d5
soobin-k <tnqls27qks@naver.com>
2023-01-29 23:27:17 +0900

    [ADD] 카테고리 선택 뷰 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/ViewController/MeetingCategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MeetingCategoryViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/29.
+//
+
+final class MeetingCategoryViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+    view.backgroundColor = .systemBackground
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+}
