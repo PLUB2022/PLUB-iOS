ef44a90487010e47703cef717680475bee7ad8cb
soobin-k <tnqls27qks@naver.com>
2023-01-29 21:51:23 +0900

    [CHORE] 주석 추가

--- PLUB/Sources/Views/Meeting/CreateMeeting/ViewModel/MeetingDateViewModel.swift
+++ PLUB/Sources/Views/Meeting/CreateMeeting/ViewModel/MeetingDateViewModel.swift
@@ -91,34 +91,34 @@ final class MeetingDateViewModel {
   }
   
   func updateDate(data: MeetingDateCollectionViewCellModel) {
-    let dates = dateInputRelay.value
-    let cellDatas = dateCellData.value
+    let dates = dateInputRelay.value // 선택된 요일 ENG 값 배열 (ex: [MON, TUE...])
+    let cellDatas = dateCellData.value // collectionView 셀 데이터 (date: 요일, isSelected: 선택 여부)
     
     if data.isSelected { // CASE 1) 선택 해제
-      dateInputRelay.accept(dates.filter { $0 != data.date.fromENGToKOR() })
+      dateInputRelay.accept(dates.filter { $0 != data.date.fromENGToKOR() }) // 선택된 요일 값 삭제
       dateCellData.accept(
         cellDatas.map {
           MeetingDateCollectionViewCellModel(
             date: $0.date,
-            isSelected: $0.date == data.date ? false : $0.isSelected
+            isSelected: $0.date == data.date ? false : $0.isSelected // 선택된 요일 값만 isSelected false로 변경
           )
         }
       )
     }
-    else if data.date == "요일 무관" { // CASE 2-1) 요일 무관 선택
-      dateInputRelay.accept([data.date.fromENGToKOR()])
+    else if data.date == "요일 무관" { // CASE 2) 요일 무관 선택
+      dateInputRelay.accept([data.date.fromENGToKOR()]) // "요일 무관"만 존재
       dateCellData.accept(
         cellDatas.map {
           MeetingDateCollectionViewCellModel(
             date: $0.date,
-            isSelected: $0.date == "요일 무관" ? true : false
+            isSelected: $0.date == "요일 무관" ? true : false // "요일 무관"만 isSelected true, 나머지는 false로 변경
           )
         }
       )
     }
-    else { // CASE 2-2) 요일 무관 제외한 요일 선택
-      var filterDates = dates.filter { $0 != "요일 무관" }
-      filterDates.append(data.date.fromENGToKOR())
+    else { // CASE 3) 요일 무관 제외한 요일 선택
+      var filterDates = dates.filter { $0 != "요일 무관" } // "요일 무관" 삭제하고,
+      filterDates.append(data.date.fromENGToKOR()) // 선택된 요일 값 추가
       dateInputRelay.accept(filterDates)
       
       dateCellData.accept(
@@ -126,11 +126,11 @@ final class MeetingDateViewModel {
           return MeetingDateCollectionViewCellModel(
             date: $0.date,
             isSelected: {
-              if $0.date == "요일 무관" {
+              if $0.date == "요일 무관" { // "요일 무관"은 isSelected false
                 return false
-              } else if $0.date == data.date {
+              } else if $0.date == data.date { // 선택된 요일은 isSelected true
                 return true
-              } else {
+              } else { // 나머지 요일은 이전 값 유지
                 return $0.isSelected
               }
             }($0)
