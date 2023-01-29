278e9f279086544e10a323eba5473f00ba63bbdf
dlrjswns <leekyunjun@gmail.com>
2023-01-29 19:32:21 +0900

    [FEAT] 북마크 등록/해제 API를 위한 모델 구현

--- PLUB/Sources/Views/Home/Model/Recruitment/RequestBookmarkResponse.swift
+++ PLUB/Sources/Views/Home/Model/Recruitment/RequestBookmarkResponse.swift
@@ -8,5 +8,18 @@
 import Foundation
 
 struct RequestBookmarkResponse: Codable {
+  let plubbingID: Int
+  let isBookmarked: Bool
   
+  enum CodingKeys: String, CodingKey {
+    case plubbingID = "plubbingId"
+    case isBookmarked
+  }
+  
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    
+    self.plubbingID = try values.decodeIfPresent(Int.self, forKey: .plubbingID) ?? -1
+    self.isBookmarked = try values.decodeIfPresent(Bool.self, forKey: .isBookmarked) ?? false
+  }
 }
