d97dd058438451dfc36015e53f1972e6498d7199
soobin-k <tnqls27qks@naver.com>
2023-01-29 21:12:14 +0900

    [REFACTOR] Day enum lower camel case로 변경

--- PLUB/Sources/Views/Meeting/CreateMeeting/ViewModel/MeetingDateViewModel.swift
+++ PLUB/Sources/Views/Meeting/CreateMeeting/ViewModel/MeetingDateViewModel.swift
@@ -8,36 +8,15 @@
 import RxSwift
 import RxCocoa
 
-enum Day: CaseIterable {
-  case MON
-  case TUE
-  case WED
-  case THR
-  case FRI
-  case SAT
-  case SUN
-  case ALL
-  
-  var toKOR: String {
-    switch self {
-    case .MON:
-      return "월"
-    case .TUE:
-      return "화"
-    case .WED:
-      return "수"
-    case .THR:
-      return "목"
-    case .FRI:
-      return "금"
-    case .SAT:
-      return "토"
-    case .SUN:
-      return "일"
-    case .ALL:
-      return "요일 무관"
-    }
-  }
+enum Day: String, CaseIterable {
+  case monday = "월"
+  case tuesday = "화"
+  case wednesday = "수"
+  case thursday = "목"
+  case friday = "금"
+  case saturday = "토"
+  case sunday = "일"
+  case all = "요일 무관"
 }
 
 final class MeetingDateViewModel {
@@ -57,7 +36,7 @@ final class MeetingDateViewModel {
   
   init() {
     let dateList = Day.allCases
-      .map { $0.toKOR }
+      .map { $0.rawValue }
       .map {
         MeetingDateCollectionViewCellModel(
           date: $0,
@@ -95,7 +74,7 @@ final class MeetingDateViewModel {
     let cellDatas = dateCellData.value
     
     if data.isSelected { // CASE 1) 선택 해제
-      dateInputRelay.accept(dates.filter { $0 != data.date.fromENGToKOR() })
+      dateInputRelay.accept(dates.filter { $0 != data.date })
       dateCellData.accept(
         cellDatas.map {
           MeetingDateCollectionViewCellModel(
@@ -106,7 +85,7 @@ final class MeetingDateViewModel {
       )
     }
     else if data.date == "요일 무관" { // CASE 2-1) 요일 무관 선택
-      dateInputRelay.accept([data.date.fromENGToKOR()])
+      dateInputRelay.accept([data.date])
       dateCellData.accept(
         cellDatas.map {
           MeetingDateCollectionViewCellModel(
@@ -117,24 +96,23 @@ final class MeetingDateViewModel {
       )
     }
     else { // CASE 2-2) 요일 무관 제외한 요일 선택
-      var filterDates = dates.filter { $0 != "ALL" }
-      filterDates.append(data.date.fromENGToKOR())
+      var filterDates = dates.filter { $0 != "요일 무관" }
+      filterDates.append(data.date)
       dateInputRelay.accept(filterDates)
       
       dateCellData.accept(
         cellDatas.map {
-          var isSelected = false
-          if $0.date == "요일 무관" {
-            isSelected = false
-          } else if $0.date == data.date {
-            isSelected = true
-          } else {
-            isSelected = $0.isSelected
-          }
-          
           return MeetingDateCollectionViewCellModel(
             date: $0.date,
-            isSelected: isSelected
+            isSelected: {
+              if $0.date == "요일 무관" {
+                return false
+              } else if $0.date == data.date {
+                return true
+              } else {
+                return $0.isSelected
+              }
+            }($0)
           )
         }
       )
