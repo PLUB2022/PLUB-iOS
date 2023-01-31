27b3de57bc69697a7b8e933ce0cbb0fa49a4b170
dlrjswns <leekyunjun@gmail.com>
2023-01-31 11:29:46 +0900

    [FEAT] SortButton 컴포넌트 추가

--- PLUB/Sources/Views/Home/Component/SortButton.swift
+++ PLUB/Sources/Views/Home/Component/SortButton.swift
@@ -10,16 +10,59 @@ import UIKit
 import SnapKit
 import Then
 
+enum SortType {
+  case popular
+  case new
+  
+  var text: String {
+    switch self {
+    case .new:
+      return "최신순"
+    case .popular:
+      return "인기순"
+    }
+  }
+}
+
 class SortButton: UIView {
   
+  private let type: SortType
   
+  private let sortLabel = UILabel().then {
+    $0.text = "인기순"
+    $0.font = .caption
+    $0.textColor = .main
+    $0.sizeToFit()
+  }
   
-  override init(frame: CGRect) {
-    super.init(frame: frame)
+  private let sortImageView = UIImageView().then {
+    $0.image = UIImage(named: "sort")
+    $0.contentMode = .scaleAspectFill
+  }
+  
+  init(type: SortType) {
+    self.type = type
+    super.init(frame: .zero)
     configureUI()
   }
   
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   private func configureUI() {
+    [sortLabel, sortImageView].forEach { addSubview($0) }
     
+    sortLabel.snp.makeConstraints { make in
+      make.centerY.equalToSuperview()
+      make.left.equalToSuperview().inset(10)
+    }
+    
+    sortImageView.snp.makeConstraints { make in
+      make.top.bottom.right.equalToSuperview()
+      make.left.equalTo(sortLabel.snp.right)
+    }
+    
+    sortLabel.text = type.text
   }
 }
