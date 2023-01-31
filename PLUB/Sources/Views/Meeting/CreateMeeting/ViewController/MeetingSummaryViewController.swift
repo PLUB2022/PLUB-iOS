22cb2f7c20a338f9011972aec814e0ba8d95d72e
soobin-k <tnqls27qks@naver.com>
2023-02-01 06:06:31 +0900

    [ADD] 모임 생성 요약 뷰 추가

---
+++ PLUB/Sources/Views/Meeting/CreateMeeting/ViewController/MeetingSummaryViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MeetingSummaryViewController.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/01/31.
+//
+
+import UIKit
+
+final class MeetingSummaryViewController: BaseViewController {
+  
+  
+  // MARK: - Life Cycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+    view.backgroundColor = .systemBackground
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+}
