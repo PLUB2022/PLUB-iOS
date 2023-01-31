9b4746e7935426171d03f438864d5f2cf04d9d2c
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-31 16:38:05 +0900

    [ADD] OnboardingViewController

---
+++ PLUB/Sources/Views/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/01/31.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class OnboardingViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
