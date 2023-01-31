bfbf6b9a39700d176c89abe6334ad6a784b95533
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-01-31 17:39:53 +0900

    [ADD] OnboardingViewModel

---
+++ PLUB/Sources/Views/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  OnboardingViewModel.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/01/31.
+//
+
+import RxCocoa
+import RxSwift
+
+final class OnboardingViewModel {
+  
+  private let titles = ["취향을 함께", "성장을 함께", "친구와 함께"]
+  private let descriptions = [
+    "좋아하는 것들을\n함께 나눌 사람들을 찾아보아요",
+    "같은 목표를 가진 사람들과 꾸준하고\n깊이 있는 소통을 하며 발전할 수 있어요",
+    "주변 친구들과 모임을 만들어\n편하게 관리하고 즐거운 추억을 쌓을 수 있어요"
+  ]
+}
