c11ca98d064a4dd0aefc8ceb710f77cbce9b5260
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-01 22:33:44 +0900

    [ADD] Logo ImageView

--- PLUB/Sources/Views/Splash/SplashViewController.swift
+++ PLUB/Sources/Views/Splash/SplashViewController.swift
@@ -12,7 +12,26 @@ import Then
 
 final class SplashViewController: BaseViewController {
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
+  /// PLUB Image
+  private let imageView = UIImageView().then {
+    $0.image = UIImage(named: "Logo")
+    $0.contentMode = .scaleAspectFit
+  }
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    view.addSubview(imageView)
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    imageView.snp.makeConstraints {
+      $0.center.equalToSuperview()
+      $0.directionalHorizontalEdges.equalToSuperview().inset(94)
+    }
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
   }
 }
