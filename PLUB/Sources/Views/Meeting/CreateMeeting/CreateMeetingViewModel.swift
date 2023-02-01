d0d1f0eee6abbba09396cb50cca73840b74b7931
soobin-k <tnqls27qks@naver.com>
2023-02-01 21:53:42 +0900

    [CHORE] pr 리뷰 반영

--- PLUB/Sources/Views/Meeting/CreateMeeting/CreateMeetingViewModel.swift
+++ PLUB/Sources/Views/Meeting/CreateMeeting/CreateMeetingViewModel.swift
@@ -24,40 +24,40 @@ final class CreateMeetingViewModel {
   private let categoryIDsRelay = BehaviorRelay<[Int]>(value: [])
   
   /// 플러빙 타이틀
-  private var titleRelay = BehaviorRelay<String>(value: "")
+  private let titleRelay = BehaviorRelay<String>(value: "")
   
   /// 플러빙 이름
-  private var nameRelay = BehaviorRelay<String>(value: "")
+  private let nameRelay = BehaviorRelay<String>(value: "")
   
   /// 플러빙 목표
-  private var goalRelay = BehaviorRelay<String>(value: "")
+  private let goalRelay = BehaviorRelay<String>(value: "")
   
   /// 소개
-  private var introduceRelay = BehaviorRelay<String>(value: "")
+  private let introduceRelay = BehaviorRelay<String>(value: "")
   
   /// 메인 이미지
-  private var mainImageRelay = BehaviorRelay<UIImage?>(value: nil)
+  private let mainImageRelay = BehaviorRelay<UIImage?>(value: nil)
   
   /// 시간
-  private var timeRelay = BehaviorRelay<String>(value: "")
+  private let timeRelay = BehaviorRelay<String>(value: "")
   
   /// 요일
-  private var daysRelay = BehaviorRelay<[String]>(value: [])
+  private let daysRelay = BehaviorRelay<[String]>(value: [])
   
   /// 온/오프라인
-  private var onOffRelay = BehaviorRelay<OnOff>(value: .on)
+  private let onOffRelay = BehaviorRelay<OnOff>(value: .on)
   
   /// 장소
-  private var locationRelay = BehaviorRelay<Location?>(value: nil)
+  private let locationRelay = BehaviorRelay<Location?>(value: nil)
   
   /// 인원 수
-  private var peopleNumberRelay = BehaviorRelay<Int>(value: 4)
+  private let peopleNumberRelay = BehaviorRelay<Int>(value: 4)
   
   /// 질문 여부
-  private var questionOnOffRelay = BehaviorRelay<Bool>(value: true)
+  private let questionOnOffRelay = BehaviorRelay<Bool>(value: true)
   
   /// 질문 리스트
-  private var questionsRelay = BehaviorRelay<[String]>(value: [])
+  private let questionsRelay = BehaviorRelay<[String]>(value: [])
   
   init() {
     categoryViewModel.selectedSubCategories
@@ -66,63 +66,51 @@ final class CreateMeetingViewModel {
       .disposed(by: disposeBag)
     
     nameViewModel.introduceTitleInputRelay
-      .asObservable()
       .bind(to: titleRelay)
       .disposed(by: disposeBag)
     
     nameViewModel.nameTitleInputRelay
-      .asObservable()
       .bind(to: nameRelay)
       .disposed(by: disposeBag)
     
     introduceViewModel.goalInputRelay
-      .asObservable()
       .bind(to: goalRelay)
       .disposed(by: disposeBag)
     
     introduceViewModel.introduceInputRelay
-      .asObservable()
       .bind(to: introduceRelay)
       .disposed(by: disposeBag)
     
     introduceViewModel.imageInputRelay
-      .asObservable()
       .bind(to: mainImageRelay)
       .disposed(by: disposeBag)
     
     dateViewModel.dateInputRelay
-      .asObservable()
       .bind(to: daysRelay)
       .disposed(by: disposeBag)
     
     dateViewModel.timeInputRelay
-      .asObservable()
       .bind(to: timeRelay)
       .disposed(by: disposeBag)
     
     dateViewModel.onOffInputRelay
-      .asObservable()
       .bind(to: onOffRelay)
       .disposed(by: disposeBag)
     
     dateViewModel.locationInputRelay
-      .asObservable()
       .bind(to: locationRelay)
       .disposed(by: disposeBag)
     
     peopleNumberViewModel.peopleNumber
-      .asObservable()
       .bind(to: peopleNumberRelay)
       .disposed(by: disposeBag)
     
     questionViewModel.noQuestionMode
-      .asObservable()
       .map { !$0 }
       .bind(to: questionOnOffRelay)
       .disposed(by: disposeBag)
     
     questionViewModel.questionListBehaviorRelay
-      .asObservable()
       .map { $0.map { $0.question } }
       .bind(to: questionsRelay)
       .disposed(by: disposeBag)
