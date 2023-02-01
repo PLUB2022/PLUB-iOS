7bb4eef95982a965d5deb870a8008e2411bae72d
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-01 22:19:22 +0900

    [ADD] SplashViewController

---
+++ PLUB/Sources/Views/Splash/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplashViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/02/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SplashViewController: BaseViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
