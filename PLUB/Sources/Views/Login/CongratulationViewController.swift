625a0ff2c10d7ffe19e07c0b0c5b4d9667b686a9
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-02 13:10:48 +0900

    [ADD] Congratuation ViewController

---
+++ PLUB/Sources/Views/Login/CongratulationViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CongratulationViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/02/02.
+//
+
+import UIKit
+
+import Lottie
+
+final class CongratulationViewController: BaseViewController {
+  
+}
