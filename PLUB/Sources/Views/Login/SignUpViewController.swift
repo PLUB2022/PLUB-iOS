43b3691a72a2c22eee4fb60aeddc7bfd0a5b1491
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-02 23:15:55 +0900

    [CHORE] '다음에 하기' 버튼 클릭시 축하페이지로 넘어가도록 수정

--- PLUB/Sources/Views/Login/SignUpViewController.swift
+++ PLUB/Sources/Views/Login/SignUpViewController.swift
@@ -262,7 +262,7 @@ final class SignUpViewController: BaseViewController {
         self.viewModel.signUp()
           .subscribe(onNext: { succeed in
             if succeed {
-              self.navigationController?.setViewControllers([HomeViewController(viewModel: HomeViewModel())], animated: true)
+              self.navigationController?.setViewControllers([CongratulationViewController()], animated: true)
             } else {
               print("회원가입 실패")
             }
