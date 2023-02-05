f1373dd617324a5b41862e7e777111215281fc69
dlrjswns <leekyunjun@gmail.com>
2023-02-06 00:28:58 +0900

    [ADD] NoResultSearchView 추가

---
+++ PLUB/Sources/Views/Home/Component/NoResultSearchView.swift
@@ -0,0 +1,20 @@
+//
+//  NoResultSearchView.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/06.
+//
+
+import UIKit
+
+final class NoResultSearchView: UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
