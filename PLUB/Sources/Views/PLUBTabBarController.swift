fd1681c377ac3ba4c5f5960473503e05171bb28c
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-11 23:12:12 +0900

    [ADD] PlubTabBarController

---
+++ PLUB/Sources/Views/PLUBTabBarController.swift
@@ -0,0 +1,33 @@
+//
+//  PLUBTabBarController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/02/07.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PLUBTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+  
+  private func setupLayouts() {
+    
+  }
+  
+  private func setupConstraints() {
+    
+  }
+  
+  private func setupStyles() {
+    
+  }
+}
