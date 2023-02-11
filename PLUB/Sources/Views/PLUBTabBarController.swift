ebd7605891464948003dd04b36bcf3606b07394d
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-11 23:18:09 +0900

    [ADD] ViewControllers in TabBarController
    - 홈, 내 모임, 알림, 프로필 탭바 생성

--- PLUB/Sources/Views/PLUBTabBarController.swift
+++ PLUB/Sources/Views/PLUBTabBarController.swift
@@ -20,7 +20,28 @@ final class PLUBTabBarController: UITabBarController {
   }
   
   private func setupLayouts() {
-    
+    viewControllers = [
+      UINavigationController(
+        rootViewController: HomeViewController(viewModel: HomeViewModel()).then {
+          $0.tabBarItem = UITabBarItem(title: "메인", image: UIImage(named: "house24"), tag: 0)
+        }
+      ),
+      UINavigationController(
+        rootViewController: MeetingViewController().then {
+          $0.tabBarItem = UITabBarItem(title: "내 모임", image: UIImage(named: "peopleCommunity24"), tag: 1)
+        }
+      ),
+      UINavigationController(
+        rootViewController: UIViewController().then {
+          $0.tabBarItem = UITabBarItem(title: "알림", image: UIImage(named: "bell24"), tag: 2)
+        }
+      ),
+      UINavigationController(
+        rootViewController: UIViewController().then {
+          $0.tabBarItem = UITabBarItem(title: "프로필", image: UIImage(named: "personOutline24"), tag: 3)
+        }
+      )
+    ]
   }
   
   private func setupConstraints() {
