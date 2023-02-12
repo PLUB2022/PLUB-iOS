48b13a71ac88b1ed4beb0defa8e6352510baf8b7
dlrjswns <leekyunjun@gmail.com>
2023-02-12 18:42:02 +0900

    [ADD] 모집글 필터를 위한 화면 파일추가

---
+++ PLUB/Sources/Views/Home/RecruitmentFilterViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RecruitmentFilterViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/12.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class RecruitmentFilterViewController: BaseViewController {
+  
+}
