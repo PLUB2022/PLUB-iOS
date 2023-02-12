194fe2c4336d73c9764175f709e5bde0052433b9
dlrjswns <leekyunjun@gmail.com>
2023-02-12 23:31:33 +0900

    [ADD] 필터화면에 필요한 슬라이더 파일 추가

---
+++ PLUB/Sources/Views/Home/Component/RecruitmentFilterSlider.swift
@@ -0,0 +1,15 @@
+//
+//  RecruitmentFilterSlider.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/12.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class RecruitmentFilterSlider: UIView {
+  
+}
