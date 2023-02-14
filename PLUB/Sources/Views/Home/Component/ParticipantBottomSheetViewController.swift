2c78248c3055e899042001204492f70ec24c41ec
dlrjswns <leekyunjun@gmail.com>
2023-02-14 20:32:35 +0900

    [ADD] 모집관련 참여자에 대한 바텀시트화면파일추가

---
+++ PLUB/Sources/Views/Home/Component/ParticipantBottomSheetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ParticipantBottomSheetViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/14.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class ParticipantBottomSheetViewController: BottomSheetViewController {
+  
+}
