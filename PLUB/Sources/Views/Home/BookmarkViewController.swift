fa682c46e275c28fc491ac6476300d21be541c1d
dlrjswns <leekyunjun@gmail.com>
2023-02-16 19:53:43 +0900

    [ADD] 북마크화면 파일 추가

---
+++ PLUB/Sources/Views/Home/BookmarkViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BookmarkViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/16.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BookmarkViewController: BaseViewController {
+  
+}
