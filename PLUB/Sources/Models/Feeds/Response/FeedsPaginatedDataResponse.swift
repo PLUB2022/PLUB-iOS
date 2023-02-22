1083bdde7102428c11f4b759fd9364289ccf236f
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-22 16:55:07 +0900

    [ADD] FeedsPaginatedDataResponse
    게시글 별 댓글 조회, 게시판 조회에 대한 API에서 사용되는 응답모델입니다.
    content 모델만 다를 뿐 나머지는 동일하기 때문에 generic으로 세팅해두었습니다.

---
+++ PLUB/Sources/Models/Feeds/Response/FeedsPaginatedDataResponse.swift
@@ -0,0 +1,37 @@
+//
+//  FeedsPaginatedDataResponse.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/02/22.
+//
+
+import Foundation
+
+/// 게시글 별 댓글 조회, 게시판 조회에 같은 응답모델이 들어감
+struct FeedsPaginatedDataResponse<FeedsModel>: Codable where FeedsModel: Codable {
+  let data: PaginatedData<FeedsModel>
+}
+
+extension FeedsPaginatedDataResponse {
+  struct PaginatedData<FeedsModel>: Codable where FeedsModel: Codable {
+    
+    /// 총 페이지 수
+    let totalPages: Int
+    
+    /// 총 데이터 개수
+    let totalElements: Int
+    
+    /// 마지막 페이지 여부
+    let isLast: Bool
+    
+    /// 데이터
+    let content: [FeedsModel]
+    
+    enum CodingKeys: String, CodingKey {
+      case totalPages
+      case totalElements
+      case content
+      case isLast = "last"
+    }
+  }
+}
