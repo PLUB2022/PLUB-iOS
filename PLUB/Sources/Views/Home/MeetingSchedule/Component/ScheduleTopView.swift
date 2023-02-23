4b40513b336ab15df664986bbe4a040851083bef
soobin-k <tnqls27qks@naver.com>
2023-02-24 02:32:31 +0900

    [ADD] 모임 일정 상단 뷰 UI 구현

---
+++ PLUB/Sources/Views/Home/MeetingSchedule/Component/ScheduleTopView.swift
@@ -0,0 +1,51 @@
+//
+//  ScheduleTopView.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/02/24.
+//
+
+import UIKit
+
+import RxCocoa
+import RxSwift
+
+final class ScheduleTopView: UIView {
+  
+  private let titleLabel = UILabel().then {
+    $0.text = "모임일정"
+    $0.font = .h3
+  }
+  
+  private let addScheduleControl = AddScheduleControl()
+  
+  init() {
+    super.init(frame: .zero)
+    setupLayouts()
+    setupConstraints()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func setupLayouts() {
+    [titleLabel, addScheduleControl].forEach {
+      addSubview($0)
+    }
+  }
+
+  private func setupConstraints() {
+    titleLabel.snp.makeConstraints {
+      $0.leading.equalToSuperview().inset(16)
+      $0.centerY.equalToSuperview()
+      $0.height.equalTo(32)
+    }
+    
+    addScheduleControl.snp.makeConstraints {
+      $0.trailing.equalToSuperview().inset(16)
+      $0.centerY.equalToSuperview()
+      $0.height.equalTo(32)
+    }
+  }
+}
--- PLUB/Sources/Views/Home/MeetingSchedule/MeetingScheduleViewController.swift
+++ PLUB/Sources/Views/Home/MeetingSchedule/MeetingScheduleViewController.swift
@@ -11,5 +11,55 @@ import SnapKit
 import Then
 
 final class MeetingScheduleViewController: BaseViewController {
+  private let scheduleTopView = ScheduleTopView()
+  private let tableView = UITableView()
   
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    [scheduleTopView, tableView].forEach {
+      view.addSubview($0)
+    }
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    scheduleTopView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.leading.trailing.equalToSuperview()
+      $0.height.equalTo(32)
+    }
+    
+    tableView.snp.makeConstraints {
+      $0.top.equalTo(scheduleTopView.snp.bottom)
+      $0.leading.trailing.bottom.equalToSuperview()
+    }
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+    setupNavigationBar()
+  }
+  
+  override func bind() {
+    super.bind()
+  }
+  
+  private func setupNavigationBar() {
+    title = "요란한 한줄"
+    navigationItem.leftBarButtonItem = UIBarButtonItem(
+      image: UIImage(named: "backButton"),
+      style: .plain,
+      target: self,
+      action: #selector(didTappedBackButton)
+    )
+  }
+  
+  @objc
+  private func didTappedBackButton() {
+    navigationController?.popViewController(animated: true)
+  }
 }
