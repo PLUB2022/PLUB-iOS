cc8cd2047567cd490317f5a861261375a88a86eb
dlrjswns <leekyunjun@gmail.com>
2023-02-24 19:32:51 +0900

    [ADD] 모집필터 뷰 모델 파일 추가

---
+++ PLUB/Sources/Views/Home/ViewModel/RecruitmentFilterViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  RecruitmentFilterViewModel.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/02/24.
+//
+
+import Foundation
+
+protocol RecruitmentFilterViewModelType {
+  
+}
+
+final class RecruitmentFilterViewModel: RecruitmentFilterViewModelType {
+  init() {
+    
+  }
+}
