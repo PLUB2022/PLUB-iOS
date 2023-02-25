96beef216b6b1f3c93c3a09810593add29fbcd16
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-25 14:54:42 +0900

    [ADD] Clipboard Scenes

--- PLUB/Sources/Views/Home/Clipboard/ClipboardViewController.swift
+++ PLUB/Sources/Views/Home/Clipboard/ClipboardViewController.swift
@@ -7,9 +7,50 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
 import SnapKit
 import Then
 
 final class ClipboardViewController: BaseViewController {
   
+  // MARK: - Properties
+  
+  private let viewModel: ClipboardViewModelType
+  
+  
+  // MARK: - Initializations
+  
+  init(viewModel: ClipboardViewModelType) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycles
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+  }
+  
+  override func bind() {
+    super.bind()
+  }
 }
