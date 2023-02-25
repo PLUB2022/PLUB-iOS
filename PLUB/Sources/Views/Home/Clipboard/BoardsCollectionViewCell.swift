38c52b8864212869351cc90c89eb7334db58c95d
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-02-25 16:00:32 +0900

    [ADD] BoardsCollectionViewCell

---
+++ PLUB/Sources/Views/Home/Clipboard/BoardsCollectionViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BoardsCollectionViewCell.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/02/25.
+//
+
+import UIKit
+
+final class BoardsCollectionViewCell: UICollectionViewCell {
+  
+  static let identifier = "BoardsCollectionViewCell"
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
