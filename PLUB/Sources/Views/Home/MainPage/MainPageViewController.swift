6f20c3001b3add9331922ef40dcb7c0464a9c8ad
dlrjswns <leekyunjun@gmail.com>
2023-03-01 16:27:15 +0900

    [ADD] 메인페이지 화면 파일 추가

---
+++ PLUB/Sources/Views/Home/MainPage/MainPageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainPageViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/03/01.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MainPageViewController: BaseViewController {
+  
+}
