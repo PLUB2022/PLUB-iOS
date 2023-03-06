78785e2583ad86f9f849fea073f48a29e7f8d7b0
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-06 12:19:41 +0900

    [CHORE] FeedsPaginatedDataResponse's properties

--- PLUB/Sources/Models/Feeds/Response/FeedsPaginatedDataResponse.swift
+++ PLUB/Sources/Models/Feeds/Response/FeedsPaginatedDataResponse.swift
@@ -10,14 +10,11 @@ import Foundation
 /// 게시글 별 댓글 조회, 게시판 조회에 같은 응답모델이 들어감
 struct FeedsPaginatedDataResponse<FeedsModel: Codable>: Codable {
   
-  /// 총 페이지 수
-  let totalPages: Int
-  
   /// 총 데이터 개수
   let totalElements: Int
   
-  /// 다음에 보내야할 `Cursor ID`
-  let nextCursorID: Int
+  /// 다음에 보내야할 `Cursor ID`, `isLast`가 `true`인 경우 `nil`값으로 대체됩니다.
+  let nextCursorID: Int?
   
   /// 마지막 페이지 여부
   let isLast: Bool
@@ -26,7 +23,6 @@ struct FeedsPaginatedDataResponse<FeedsModel: Codable>: Codable {
   let content: [FeedsModel]
   
   enum CodingKeys: String, CodingKey {
-    case totalPages
     case totalElements
     case content
     case nextCursorID = "nextCursorId"
