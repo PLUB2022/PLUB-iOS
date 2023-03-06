02f82250782330ce3d1064061475b3fb6435e9d2
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-06 17:05:10 +0900

    [ADD] BoardDetailCollectionViewCell

---
+++ PLUB/Sources/Views/Home/Clipboard/BoardDetailCollectionViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  BoardDetailCollectionViewCell.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/03/06.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BoardDetailCollectionViewCell: UICollectionViewCell {
+  
+  // MARK: - UI Components
+  
+  // MARK: - Initializations
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setupLayouts()
+    setupConstraints()
+    setupStyles()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Configurations
+  
+  private func setupLayouts() {
+    
+  }
+  
+  private func setupConstraints() {
+    
+  }
+  
+  private func setupStyles() {
+    
+  }
+}
