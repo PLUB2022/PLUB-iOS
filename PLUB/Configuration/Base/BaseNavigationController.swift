70d9ac20e05d785c83e03f6fee8ae378bd70a1b7
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-10 15:24:02 +0900

    [ADD] BaseNavigationController

---
+++ PLUB/Configuration/Base/BaseNavigationController.swift
@@ -0,0 +1,15 @@
+//
+//  BaseNavigationController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/03/10.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
