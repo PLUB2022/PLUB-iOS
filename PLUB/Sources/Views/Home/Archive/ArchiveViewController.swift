6568a5b687371802b0396055c9d007bfee440dc0
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-10 16:47:27 +0900

    [ADD] Archive ViewController(ViewModel)

--- PLUB/Sources/Views/Home/Archive/ArchiveViewController.swift
+++ PLUB/Sources/Views/Home/Archive/ArchiveViewController.swift
@@ -2,14 +2,55 @@
 //  ArchiveViewController.swift
 //  PLUB
 //
-//  Created by 이건준 on 2023/02/20.
+//  Created by 홍승현 on 2023/03/10.
 //
 
 import UIKit
 
+import RxSwift
+import RxCocoa
 import SnapKit
 import Then
 
 final class ArchiveViewController: BaseViewController {
   
+  // MARK: - Properties
+  
+  private let viewModel: ArchiveViewModelType
+  
+  
+  // MARK: - Initializations
+  
+  init(viewModel: ArchiveViewModelType) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycles
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - Configuration
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+  }
+  
+  override func bind() {
+    super.bind()
+  }
 }
