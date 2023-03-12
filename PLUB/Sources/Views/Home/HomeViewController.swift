588c02a0604745f00133fe4fa8e9fbef2e68e217
dlrjswns <leekyunjun@gmail.com>
2023-03-12 14:17:24 +0900

    [CHORE] 메인페이지화면이동코드 추가

--- PLUB/Sources/Views/Home/HomeViewController.swift
+++ PLUB/Sources/Views/Home/HomeViewController.swift
@@ -165,13 +165,10 @@ class HomeViewController: BaseViewController {
   }
   
   @objc private func didTappedBookmarkButton() {
-    let vc = MainPageViewController()
+    let vc = BookmarkViewController()
+    vc.title = "북마크"
     vc.navigationItem.largeTitleDisplayMode = .never
     self.navigationController?.pushViewController(vc, animated: true)
-//    let vc = BookmarkViewController()
-//    vc.title = "북마크"
-//    vc.navigationItem.largeTitleDisplayMode = .never
-//    self.navigationController?.pushViewController(vc, animated: true)
   }
   
   private func createCompositionalSection(homeCollectionType: HomeSectionType) -> NSCollectionLayoutSection {
