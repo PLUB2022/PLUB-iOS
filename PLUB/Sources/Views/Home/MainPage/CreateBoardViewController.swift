c863a88d7a87b7d6b2dfbd2a31272b578cdcf099
dlrjswns <leekyunjun@gmail.com>
2023-03-17 23:00:34 +0900

    [ADD] CreateBoardViewController 파일 생성

---
+++ PLUB/Sources/Views/Home/MainPage/CreateBoardViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreateBoardViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/03/17.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class CreateBoardViewController: UIViewController {
+  
+}
