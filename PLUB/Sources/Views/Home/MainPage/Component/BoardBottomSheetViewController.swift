d4bf74d7d8a8e8bb99bfd558797de4175bddcfc7
dlrjswns <leekyunjun@gmail.com>
2023-03-20 13:52:15 +0900

    [ADD] 게시판 롱프레스전용 바텀시트 파일 추가

---
+++ PLUB/Sources/Views/Home/MainPage/Component/BoardBottomSheetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BoardBottomSheetViewController.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/03/20.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class BoardBottomSheetViewController: BottomSheetViewController {
+  
+}
