49150d8d291233a9032744071a95ee2a5779e8c0
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-22 17:47:17 +0900

    [CHORE] CommentContent's Property Comments

--- PLUB/Sources/Models/Feeds/CommentContent.swift
+++ PLUB/Sources/Models/Feeds/CommentContent.swift
@@ -33,9 +33,15 @@ struct CommentContent: Codable {
   let nickname: String
   
   /// 댓글 작성 날짜
+  ///
+  /// `2023-03-19 20:24:56`형태의 String 값이 들어옵니다.
   let postDate: String
   
-  /// 댓글 작성자 여부
+  /// 해당 댓글의 작성자가 자신인지 판단하는 변수입니다.
+  ///
+  /// 원 게시글 저자인 경우, `isAuthorComment`와 동일한 역할을 하게 됩니다. 하지만 다르게 사용해야함을 명심해주세요.
+  /// 본 게시글 저자가 아닌 경우, `isCommentAuthor`은 자신의 댓글에서만 true값으로 응답되지만, `isAuthorComment`은
+  /// 현 게시글의 작성자가 단 댓글에서만 `true`값이 나오게 됩니다.
   let isCommentAuthor: Bool
   
   /// 현재 앱으로 로그인한 사용자가 해당 게시물의 저자인지를 나타냅니다. 만약 그렇다면, 모든 댓글 정보에 대한 이 변수값은 `true`로 세팅되고, 아닐 경우 `false`로 세팅됩니다.
