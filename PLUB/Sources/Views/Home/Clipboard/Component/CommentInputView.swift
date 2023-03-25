5cac284bc36a19b2b2042438fdeee73ca32ee723
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-25 20:30:19 +0900

    [ADD] CommentInputView

---
+++ PLUB/Sources/Views/Home/Clipboard/Component/CommentInputView.swift
@@ -0,0 +1,48 @@
+//
+//  CommentTextView.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/03/25.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+import SnapKit
+import Then
+
+final class CommentInputView: UIView {
+  
+  // MARK: - Properties
+  
+  private let disposeBag = DisposeBag()
+  
+  // MARK: - UI Components
+  
+  private let containerView = UIStackView().then {
+    $0.alignment = .center
+    $0.spacing = 8
+  }
+  
+  private let textView = UITextView().then {
+    $0.textColor = .black
+    $0.font = .body1
+  }
+  
+  private let uploadButton = UIButton()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+extension CommentInputView {
+  enum Constants {
+    static let placeholder = "댓글을 입력하세요"
+  }
+}
