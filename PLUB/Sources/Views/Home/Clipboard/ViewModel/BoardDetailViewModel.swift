f8b05e3feffea0216bb32dba4d23aebb5385b877
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-03-26 18:04:19 +0900

    [MOVE] Move Diffable DataSource code to view model

--- PLUB/Sources/Views/Home/Clipboard/ViewModel/BoardDetailViewModel.swift
+++ PLUB/Sources/Views/Home/Clipboard/ViewModel/BoardDetailViewModel.swift
@@ -12,39 +12,40 @@ import RxCocoa
 
 protocol BoardDetailViewModelType {
   // Input
-  
+  var setCollectionViewObserver: AnyObserver<UICollectionView> { get }
   // Output
-  var fetchAlertDriver: Driver<Void> { get }
 }
 
 protocol BoardDetailDataStore {
   var content: BoardModel { get }
   var comments: [CommentContent] { get }
+  /// 게시글, 댓글에 대한 CollectionViewDiffableDataSource
+  var dataSource: BoardDetailViewModel.DataSource? { get }
 }
 
 final class BoardDetailViewModel: BoardDetailViewModelType, BoardDetailDataStore {
   
-  // Output
-  let fetchAlertDriver: Driver<Void>
+  // Input
+  let setCollectionViewObserver: AnyObserver<UICollectionView>
   
   // MARK: - Properties
   
   let content: BoardModel
   var comments: [CommentContent] = []
   
-  private let requestCommentsSubject = PublishSubject<Void>()
+  private(set) var dataSource: DataSource?
   
   // MARK: - Initializations
   
   init(plubbingID: Int, content: BoardModel) {
     self.content = content
     
-    fetchAlertDriver = requestCommentsSubject.asDriver(onErrorDriveWith: .empty())
-    bind(plubbingID: plubbingID)
-  }
-  
-  private func bind(plubbingID: Int) {
-    FeedsService.shared.fetchComments(plubbingID: plubbingID, feedID: content.feedID, nextCursorID: comments.last?.commentID ?? 0)
+    let collectionViewSubject = PublishSubject<UICollectionView>()
+    
+    setCollectionViewObserver = collectionViewSubject.asObserver()
+    
+    
+    let commentsObservable = FeedsService.shared.fetchComments(plubbingID: plubbingID, feedID: content.feedID, nextCursorID: comments.last?.commentID ?? 0)
       .compactMap { result -> FeedsPaginatedDataResponse<CommentContent>? in
         // TODO: 승현 - API 통신 에러 처리
         guard case let .success(response) = result else { return nil }
@@ -52,12 +53,69 @@ final class BoardDetailViewModel: BoardDetailViewModelType, BoardDetailDataStore
       }
       .filter { [weak self] in $0.isLast == false || self?.comments.count == 0 }
       .map { $0.content }
-      .subscribe(with: self) { owner, model in
-        owner.comments.append(contentsOf: model)
-        owner.requestCommentsSubject.onNext(Void()) // content, comments가 전부 할당되었으므로 onNext로 알림
+    
+    Observable.combineLatest(collectionViewSubject.asObservable(), commentsObservable)
+      .subscribe(with: self) { owner, tuple in
+        owner.comments.append(contentsOf: tuple.1)
+        owner.setCollectionView(tuple.0)
+        owner.applyInitialSnapshots()
       }
       .disposed(by: disposeBag)
   }
   
   private let disposeBag = DisposeBag()
+}
+
+extension BoardDetailViewModel {
+  
+  // MARK: Type Alias
+  
+  typealias Section = Int
+  typealias Item = CommentContent
+  typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+  typealias Snapshot = NSDiffableDataSourceSnapshot<Section, Item>
+  
+  typealias CellRegistration = UICollectionView.CellRegistration<BoardDetailCollectionViewCell, CommentContent>
+  typealias HeaderRegistration = UICollectionView.SupplementaryRegistration<BoardDetailCollectionHeaderView>
+  
+  // MARK: Snapshot & DataSource Part
+  
+  /// Collection View를 세팅하며, `DiffableDataSource`를 초기화하여 해당 Collection View에 데이터를 지닌 셀을 처리합니다.
+  private func setCollectionView(_ collectionView: UICollectionView) {
+    
+    // 단어 그대로 `등록`처리 코드, 셀 후처리할 때 사용됨
+    let registration = CellRegistration { cell, _, item in
+      cell.configure(with: item)
+    }
+    
+    // Header View Registration, 헤더 뷰 후처리에 사용됨
+    let headerRegistration = HeaderRegistration(elementKind: UICollectionView.elementKindSectionHeader) { [content] supplementaryView, elementKind, indexPath in
+      supplementaryView.configure(with: content)
+    }
+    
+    // dataSource에 cell 등록
+    dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, item in
+      return collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: item)
+    }
+    
+    // dataSource에 headerView도 등록
+    dataSource?.supplementaryViewProvider = .init { collectionView, elementKind, indexPath in
+      return collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
+    }
+  }
+  
+  /// 초기 Snapshot을 설정합니다. DataSource가 초기화될 시 해당 메서드가 실행됩니다.
+  /// 직접 이 메서드를 실행할 필요는 없습니다.
+  private func applyInitialSnapshots() {
+    var snapshot = Snapshot()
+    
+    var sections = [-1] // 최소한 하나의 Section이라도 존재해야 함
+    sections.append(contentsOf: Array(Set(comments.map { $0.groupID })).sorted())
+    snapshot.appendSections(sections)
+    
+    sections.forEach { sectionGroupID in
+      snapshot.appendItems(comments.filter { $0.groupID == sectionGroupID }, toSection: sectionGroupID)
+    }
+    dataSource?.apply(snapshot)
+  }
 }
