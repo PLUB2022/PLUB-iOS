ae8c2f66596a50d9244e4f9455f764c08c387b83
dlrjswns <leekyunjun@gmail.com>
2023-03-28 15:51:50 +0900

    [ADD] 투두리스트화면 라벨 및 컬렉션뷰 추가

--- PLUB/Sources/Views/Home/MainPage/TodolistViewController.swift
+++ PLUB/Sources/Views/Home/MainPage/TodolistViewController.swift
@@ -11,8 +11,38 @@ import SnapKit
 import Then
 
 final class TodolistViewController: BaseViewController {
+  
+  private let titleLabel = UILabel().then {
+    $0.font = .appFont(family: .nanum, size: 32)
+    $0.text = "“2주에 한 권씩”"
+    $0.addLineSpacing($0)
+  }
+  
+  private let todoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout()).then {
+    $0.backgroundColor = .background
+  }
+  
   override func setupStyles() {
     super.setupStyles()
-    view.backgroundColor = .blue
+    view.backgroundColor = .background
+  }
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    [titleLabel, todoCollectionView].forEach { view.addSubview($0) }
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    titleLabel.snp.makeConstraints {
+      $0.top.equalToSuperview().inset(16)
+      $0.centerX.equalToSuperview()
+      $0.height.equalTo(40)
+    }
+    
+    todoCollectionView.snp.makeConstraints {
+      $0.top.equalTo(titleLabel.snp.bottom)
+      $0.directionalHorizontalEdges.bottom.equalToSuperview()
+    }
   }
 }
