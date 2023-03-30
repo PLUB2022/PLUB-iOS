f182661957f0eea0762baa830c5e898a0c7b9451
dlrjswns <leekyunjun@gmail.com>
2023-03-30 23:50:49 +0900

    [ADD] xMarkDeepGray 이미지 에셋 추가

--- PLUB/Sources/Views/Home/MainPage/Component/TodoAlertController.swift
+++ PLUB/Sources/Views/Home/MainPage/Component/TodoAlertController.swift
@@ -11,5 +11,28 @@ import SnapKit
 import Then
 
 final class TodoAlertController: BaseViewController {
+  private let containerView = UIView().then {
+    $0.backgroundColor = .white
+    $0.layer.masksToBounds = true
+    $0.layer.cornerRadius = 10
+  }
   
+  private let profileImageView = UIImageView().then {
+    $0.layer.masksToBounds = true
+    $0.layer.cornerRadius = 8
+  }
+  
+  private let dateLabel = UILabel().then {
+    $0.font = .overLine
+    $0.textColor = .black
+  }
+  
+  private let nameLabel = UILabel().then {
+    $0.font = .caption2
+    $0.textColor = .black
+  }
+  
+  private let closeButton = UIButton().then {
+    $0.setImage(UIImage(named: ""), for: .normal)
+  }
 }
