a2f0e96c59b725c393c3c9e92dce793eaa9bc7c7
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-05 21:26:26 +0900

    [ADD] Add Comments in SearchParameter

--- PLUB/Sources/Models/Recruitment/Request/SearchParameter.swift
+++ PLUB/Sources/Models/Recruitment/Request/SearchParameter.swift
@@ -7,10 +7,22 @@
 
 import Foundation
 
+/// 모집 글을 검색할 때 query로 요청할 모델
 struct SearchParameter: Encodable {
+  
+  /// 검색한 키워드
   let keyword: String
+  
   let page: Int?
+  
+  /// 필터링 타입
+  ///
+  /// title(제목), name(모임 이름), mix(제목 + 글) 세 가지 중 하나로 기입됩니다.
   let type: String?
+  
+  /// 정렬 기준
+  ///
+  /// popular(인기순), 최신순(new) 중 하나로 기입됩니다.
   let sort: String
   
   init(keyword: String, page: Int? = 1, type: String? = "mix", sort: String = "popular") {
