57c1b98dfa6027cbc6f7b3458fd85a98dba18dd5
dlrjswns <leekyunjun@gmail.com>
2023-04-06 00:34:57 +0900

    [ADD] Content를 인자로 받는 초기화함수 추가

--- PLUB/Sources/Views/Home/Cell/SelectCategory/SelectedCategoryChartCollectionViewCell.swift
+++ PLUB/Sources/Views/Home/Cell/SelectCategory/SelectedCategoryChartCollectionViewCell.swift
@@ -19,6 +19,23 @@ struct SelectedCategoryCollectionViewCellModel { // 차트, 그리드일때 둘 
   let introduce: String
   var isBookmarked: Bool
   let selectedCategoryInfoModel: CategoryInfoListModel
+  
+  init(content: Content) {
+    plubbingID = content.plubbingID,
+    name = content.name,
+    title = content.title,
+    mainImage = content.mainImage,
+    introduce = content.introduce,
+    isBookmarked = content.isBookmarked,
+    selectedCategoryInfoModel = .init(
+      placeName = content.placeName,
+      peopleCount = content.remainAccountNum,
+      dateTime = content.days
+        .map { $0.fromENGToKOR() }
+        .joined(separator: ",")
+      + " | "
+      + "(data.time)")
+  }
 }
 
 protocol SelectedCategoryChartCollectionViewCellDelegate: AnyObject {
