b301765a062503eeee63875bdecf490f789b3685
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-05 17:03:19 +0900

    [CHORE] Add optional type in CategoryMeetingRequest
    days와 subCategoryIDs는 필수 값이 아니기에 옵셔널 타입을 추가하였습니다.

--- PLUB/Sources/Models/Meeting/Request/CategoryMeetingRequest.swift
+++ PLUB/Sources/Models/Meeting/Request/CategoryMeetingRequest.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 
+/// 카테고리별 모임 조회를 위한 요청 모델
 struct CategoryMeetingRequest: Codable {
-  let days: [String]
-  let subCategoryIDs: [Int]
+  
+  /// 필터링할 요일
+  ///
+  /// ["MON", "TUE", "WED"]와 같이 대문자 형식의 영문명 앞 세 글자만을 사용합니다.
+  /// 만약 요일무관인 경우 해당 값은 nil로 처리됩니다.
+  let days: [String]?
+  
+  /// 필터링할 상세 카테고리 Identifier
+  ///
+  /// 만약 전체 카테고리를 띄우고싶은 경우 해당 값을 nil로 처리하면 됩니다.
+  let subCategoryIDs: [Int]?
+  
+  /// 필터링할 모집 인원 수
   let accountNum: Int
   
   enum CodingKeys: String, CodingKey {
