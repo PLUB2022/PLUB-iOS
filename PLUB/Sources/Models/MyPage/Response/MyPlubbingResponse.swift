41469cff377eb2bd73b545f6190eafc55997de16
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-05 20:49:57 +0900

    [ADD] Comments
    - PlubbingStatusType
    - MyPlubbingResponse
    - MyPagePlubbing

--- PLUB/Sources/Models/MyPage/Response/MyPlubbingResponse.swift
+++ PLUB/Sources/Models/MyPage/Response/MyPlubbingResponse.swift
@@ -7,15 +7,28 @@
 
 import Foundation
 
+/// 플러빙 상태
 enum PlubbingStatusType: String, CaseIterable {
+  
+  /// 모집 중인 상태
   case recruiting = "RECRUITING"
+  
+  /// 대기 중인 상태
   case waiting = "WAITING"
+  
+  /// 활동 중인 상태
   case active = "ACTIVE"
+  
+  /// 종료된 상태
   case end = "END"
 }
 
 struct MyPlubbingResponse: Codable {
+  
+  /// 나의 플러빙 상태
   let plubbingStatus: PlubbingStatusType.RawValue
+  
+  /// 본 `plubbingStatus`값을 따르는 플러빙 리스트
   var plubbings: [MyPagePlubbing]
   
   enum CodingKeys: String, CodingKey {
@@ -25,10 +38,22 @@ struct MyPlubbingResponse: Codable {
 }
 
 struct MyPagePlubbing: Codable {
+  
+  /// 플러빙 ID
   let plubbingID: Int
+  
+  /// 플러빙 이름
   let name: String
+  
+  /// 플러빙 목표
   let goal: String
+  
+  /// 플러빙 카테고리 아이콘 이미지
   let iconImage: String
+  
+  /// 내 플러빙 상태
+  ///
+  /// `HOST`, `GUEST`, `END`값 중 하나로 들어옵니다.
   let myPlubbingStatus: String
   
   enum CodingKeys: String, CodingKey {
