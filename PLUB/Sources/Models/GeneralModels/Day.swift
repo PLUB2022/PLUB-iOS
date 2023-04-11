4d097001627dca3c5327b3df2b47c27d9ba3726d
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-11 11:05:31 +0900

    [ADD] Add initializer to `Day` enum

--- PLUB/Sources/Models/GeneralModels/Day.swift
+++ PLUB/Sources/Models/GeneralModels/Day.swift
@@ -17,16 +17,30 @@ enum Day: String, Codable, CaseIterable {
   case saturday  = "SAT"
   case sunday    = "SUN"
   
+  init?(rawValue: String) {
+    switch rawValue {
+    case "월", "MON":             self = .monday
+    case "화", "TUE":             self = .tuesday
+    case "수", "WED":             self = .wednesday
+    case "목", "THR":             self = .thursday
+    case "금", "FRI":             self = .friday
+    case "토", "SAT":             self = .saturday
+    case "일", "SUN":             self = .sunday
+    case "전체", "요일 무관", "ALL": self = .all
+    default:                     return nil
+    }
+  }
+  
   var kor: String {
     switch self {
-    case .monday: return "월"
-    case .tuesday: return "화"
-    case .wednesday: return "수"
-    case .thursday: return "목"
-    case .friday: return "금"
-    case .saturday: return "토"
-    case .sunday: return "일"
-    case .all: return "요일 무관"
+    case .monday:       return "월"
+    case .tuesday:      return "화"
+    case .wednesday:    return "수"
+    case .thursday:     return "목"
+    case .friday:       return "금"
+    case .saturday:     return "토"
+    case .sunday:       return "일"
+    case .all:          return "요일 무관"
     }
   }
 }
