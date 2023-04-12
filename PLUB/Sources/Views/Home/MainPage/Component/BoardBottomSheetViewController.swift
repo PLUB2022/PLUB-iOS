1485380ff16635c42b71ffc830b92d1d527aef20
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-12 17:05:40 +0900

    [REFACTOR] BoardBottomSheetViewController
    - grabber view 삭제
    - constraint 재구성

--- PLUB/Sources/Views/Home/MainPage/Component/BoardBottomSheetViewController.swift
+++ PLUB/Sources/Views/Home/MainPage/Component/BoardBottomSheetViewController.swift
@@ -26,15 +26,9 @@ final class BoardBottomSheetViewController: BottomSheetViewController {
   
   weak var delegate: BoardBottomSheetDelegate?
   
-  private let grabber = UIView().then {
-    $0.backgroundColor = .mediumGray
-  }
-  
   private let contentStackView = UIStackView().then {
     $0.axis = .vertical
     $0.spacing = 8
-    $0.isLayoutMarginsRelativeArrangement = true
-    $0.layoutMargins = .init(top: 24, left: 16, bottom: 24, right: 16)
   }
   
   private let clipboardFixView = PhotoBottomSheetListView(text: "클립보드에 고정", image: "pinBlack")
@@ -44,29 +38,26 @@ final class BoardBottomSheetViewController: BottomSheetViewController {
   
   override func setupLayouts() {
     super.setupLayouts()
+    contentView.addSubview(contentStackView)
+    
     [clipboardFixView, modifyBoardView, reportBoardView, deleteBoardView].forEach {
-      $0.snp.makeConstraints {
-        $0.height.equalTo(52)
-      }
       contentStackView.addArrangedSubview($0)
     }
-    
-    [grabber, contentStackView].forEach { contentView.addSubview($0) }
   }
   
   override func setupConstraints() {
     super.setupConstraints()
     
-    grabber.snp.makeConstraints {
-      $0.width.equalTo(48)
-      $0.height.equalTo(4)
-      $0.centerX.equalToSuperview()
-      $0.top.equalToSuperview().inset(8)
+    [clipboardFixView, modifyBoardView, reportBoardView, deleteBoardView].forEach {
+      $0.snp.makeConstraints {
+        $0.height.equalTo(50)
+      }
     }
     
     contentStackView.snp.makeConstraints {
-      $0.top.equalTo(grabber.snp.bottom)
-      $0.directionalHorizontalEdges.bottom.equalToSuperview()
+      $0.top.equalToSuperview().inset(36)
+      $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+      $0.bottom.equalToSuperview().inset(24)
     }
   }
   
