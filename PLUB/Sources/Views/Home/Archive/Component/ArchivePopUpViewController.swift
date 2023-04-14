c89d84d92acaa01a39a95ad202fa6d92035b0611
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-14 20:41:15 +0900

    [ADD] Add Pop up motion in ArchivePopUpViewController

--- PLUB/Sources/Views/Home/Archive/Component/ArchivePopUpViewController.swift
+++ PLUB/Sources/Views/Home/Archive/Component/ArchivePopUpViewController.swift
@@ -11,6 +11,20 @@ final class ArchivePopUpViewController: BaseViewController {
   
   // MARK: - UI Components
   
+  // MARK: - Initializations
+  
+  init() {
+    super.init(nibName: nil, bundle: nil)
+    
+    // popup motion 적용
+    modalTransitionStyle = .crossDissolve
+    modalPresentationStyle = .overFullScreen
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   // MARK: - Configurations
   
   override func setupLayouts() {
