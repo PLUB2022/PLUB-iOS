54ffbe7579b11ea55ad56c762528569b8129024a
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-14 20:27:48 +0900

    [ADD] ArchivePopUpViewController

---
+++ PLUB/Sources/Views/Home/Archive/Component/ArchivePopUpViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ArchivePopUpViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/04/14.
+//
+
+import UIKit
+
+final class ArchivePopUpViewController: BaseViewController {
+  
+  // MARK: - UI Components
+  
+  // MARK: - Configurations
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+  }
+  
+  override func setupStyles() {
+    super.setupStyles()
+  }
+}
