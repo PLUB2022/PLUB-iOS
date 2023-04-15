4bdfbb35976b44c98cc0c202e871e4c92d740e43
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-15 23:07:09 +0900

    [ADD] ArchivePopUpViewModel

---
+++ PLUB/Sources/Views/Home/Archive/ViewModel/ArchivePopUpViewModel.swift
@@ -0,0 +1,41 @@
+//
+//  ArchivePopUpViewModel.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/04/15.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+protocol ArchivePopUpViewModelType {
+  // Input
+  
+  // Output
+}
+
+final class ArchivePopUpViewModel {
+  
+  // MARK: - Properties
+  
+  
+  // MARK: - Initialization
+  
+  init(plubbingID: Int, archiveID: Int) {
+    
+  }
+  
+  private let disposeBag = DisposeBag()
+  
+  
+  // MARK: - Configuration
+  
+}
+
+// MARK: - ArchivePopUpViewModelType
+
+extension ArchivePopUpViewModel: ArchivePopUpViewModelType {
+
+}
