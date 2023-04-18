b869641372cd36ca75a751bdfc5eb8fdbdd4dae0
dlrjswns <leekyunjun@gmail.com>
2023-04-18 17:08:13 +0900

    [FEAT] 게시판 생성에 따른 이미지 업로드 및 Request요청 처리 코드 추가
    앞으로 Request처리를 VC가 아닌 ViewModel 내부에서 처리하며 게시판 생성시 필요한 이미지 데이터를 요청할 수 있습니다.

--- PLUB/Sources/Views/Home/MainPage/ViewModel/CreateBoardViewModel.swift
+++ PLUB/Sources/Views/Home/MainPage/ViewModel/CreateBoardViewModel.swift
@@ -5,17 +5,20 @@
 //  Created by 이건준 on 2023/03/18.
 //
 
+import UIKit
+
 import RxSwift
 import RxCocoa
 
 protocol CreateBoardViewModelType {
   // Input
-  var whichUpload: AnyObserver<BoardsRequest> { get }
   var selectMeeting: AnyObserver<Int> { get }
   var writeTitle: AnyObserver<String> { get }
   var writeContent: AnyObserver<String> { get }
   var isSelectImage: AnyObserver<Bool> { get }
   var whichPostType: AnyObserver<PostType> { get }
+  var whichBoardImage: AnyObserver<UIImage?> { get }
+  var tappedUploadButton: AnyObserver<Void> { get }
   
   // Output
   var isSuccessCreateBoard: Signal<Int> { get }
@@ -27,12 +30,13 @@ final class CreateBoardViewModel: CreateBoardViewModelType {
   private let disposeBag = DisposeBag()
   
   // Input
-  let whichUpload: AnyObserver<BoardsRequest>
   let selectMeeting: AnyObserver<Int>
   let writeTitle: AnyObserver<String>
   let writeContent: AnyObserver<String>
   let isSelectImage: AnyObserver<Bool>
   let whichPostType: AnyObserver<PostType>
+  let whichBoardImage: AnyObserver<UIImage?>
+  let tappedUploadButton: AnyObserver<Void>
   
   // Output
   let isSuccessCreateBoard: Signal<Int>
@@ -41,13 +45,16 @@ final class CreateBoardViewModel: CreateBoardViewModelType {
   init() {
     let whichUploadingRequest = PublishSubject<BoardsRequest>()
     let whichPlubbingID = PublishSubject<Int>()
+    let whichUploadingImage = PublishSubject<UIImage?>()
     let writingTitle = BehaviorSubject<String>(value: "")
     let writingContent = BehaviorSubject<String>(value: "")
     let isSelectingImage = BehaviorSubject<Bool>(value: false)
     let isSuccessCreatingBoard = PublishRelay<Int>()
     let selectingPostType = BehaviorSubject<PostType>(value: .photo)
+    let tappedUploadingButton = PublishSubject<Void>()
     
-    whichUpload = whichUploading.asObserver()
+    tappedUploadButton = tappedUploadingButton.asObserver()
+    whichBoardImage = whichUploadingImage.asObserver()
     selectMeeting = whichPlubbingID.asObserver()
     writeTitle = writingTitle.asObserver()
     writeContent = writingContent.asObserver()
@@ -55,7 +62,72 @@ final class CreateBoardViewModel: CreateBoardViewModelType {
     whichPostType = selectingPostType.asObserver()
     
     // Input
-    let createBoard = Observable.zip(
+    
+    let titleExceptPlaceholder = writingTitle
+      .filter { $0 != Constants.titlePlaceholder }
+    
+    let contentExceptPlaceholder = writingContent
+      .filter { $0 != Constants.contentPlaceholder }
+    
+    let titleNotEmpty = titleExceptPlaceholder
+      .map { !$0.isEmpty }
+    
+    let contentNotEmpty = contentExceptPlaceholder
+      .map { !$0.isEmpty }
+    
+    let postType = selectingPostType.share()
+    
+    let uploadImage = whichUploadingImage
+      .compactMap { $0 }
+      .flatMapLatest { image in
+        ImageService.shared.uploadImage(images: [image], params: .init(type: .feed))
+      }
+    
+    let tryImageToString = uploadImage
+      .flatMap { response -> Observable<String?> in
+        switch response {
+        case let .success(imageModel):
+          return .just(imageModel.data?.files.first?.fileURL)
+        default:
+          // 이미지 등록이 되지 못함 (오류 발생)
+          return .empty()
+        }
+      }
+    
+    tappedUploadingButton
+      .withLatestFrom(postType)
+      .flatMap { type in
+        switch type {
+        case .photo:
+          return Observable.combineLatest(
+            titleExceptPlaceholder,
+            tryImageToString
+          ) { title, feedImage in
+            return BoardsRequest(title: title, content: nil, feedImage: feedImage)
+          }
+        case .text:
+          return Observable.combineLatest(
+            titleExceptPlaceholder,
+            contentExceptPlaceholder
+          ) { title, content in
+            return BoardsRequest(title: title, content: content, feedImage: nil)
+          }
+        case .photoAndText:
+          return Observable.combineLatest(
+            titleExceptPlaceholder,
+            contentExceptPlaceholder,
+            tryImageToString
+          ) { title, content, feedImage in
+            return BoardsRequest(title: title, content: content, feedImage: feedImage)
+          }
+        }
+      }
+      .subscribe(onNext: { request in
+        whichUploadingRequest.onNext(request)
+      })
+      .disposed(by: disposeBag)
+    
+    let createBoard = Observable.combineLatest(
       whichPlubbingID,
       whichUploadingRequest
     )
@@ -72,17 +144,6 @@ final class CreateBoardViewModel: CreateBoardViewModelType {
     // Output
     isSuccessCreateBoard = isSuccessCreatingBoard
       .asSignal(onErrorSignalWith: .empty())
-    
-    let titleNotEmpty = writingTitle
-      .filter { $0 != Constants.titlePlaceholder }
-      .map { !$0.isEmpty }
-    
-    let contentNotEmpty = writingContent
-      .filter { $0 != Constants.contentPlaceholder }
-      .map { !$0.isEmpty }
-    
-    let postType = selectingPostType
-      .share()
     
     uploadButtonIsActivated = postType
       .flatMap { type in
