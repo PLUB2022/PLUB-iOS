7d1e19b1b00b4b111ac76a2a7f32973c30761143
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-22 22:55:58 +0900

    [FEAT] Implement API Binding using usecase in ArchiveUploadViewModel
    getArchiveDetailUseCase를 사용하여 fetching한 아카이브를 토대로
    diffableDataSource를 업데이트하였습니다.

--- PLUB/Sources/Views/Home/Archive/ViewModel/ArchiveUploadViewModel.swift
+++ PLUB/Sources/Views/Home/Archive/ViewModel/ArchiveUploadViewModel.swift
@@ -40,18 +40,37 @@ final class ArchiveUploadViewModel {
   init(getArchiveDetailUseCase: GetArchiveDetailUseCase) {
     self.getArchiveDetailUseCase = getArchiveDetailUseCase
     
-    setCollectionView()
+    fetchArchives()
   }
   
   private let disposeBag = DisposeBag()
 }
+
+// MARK: - Binding Methods
+
+private extension ArchiveUploadViewModel {
+  
+  /// 아카이브를 조회한 결과 값을 토대로 View를 업데이트합니다.
+  func fetchArchives() {
+    getArchiveDetailUseCase.execute()
+      .withLatestFrom(setCollectionViewSubject) {
+        (content: $0, collectionView: $1)
+      }
+      .subscribe(with: self) { owner, tuple in
+        owner.archivesContents = tuple.content.images
+        owner.setCollectionView(tuple.collectionView, titleText: tuple.content.title)
+      }
+      .disposed(by: disposeBag)
+  }
+}
+
+// MARK: - ArchiveUploadViewModelType
 
 extension ArchiveUploadViewModel: ArchiveUploadViewModelType {
   var collectionViewObserver: AnyObserver<UICollectionView> {
     setCollectionViewSubject.asObserver()
   }
 }
-
 
 // MARK: - Diffable DataSource
 
@@ -74,28 +93,25 @@ private extension ArchiveUploadViewModel {
   // MARK: Snapshot & DataSource Part
   
   /// Collection View를 세팅하며, `DiffableDataSource`를 초기화하여 해당 Collection View에 데이터를 지닌 셀을 처리합니다.
-  func setCollectionView() {
+  func setCollectionView(_ collectionView: UICollectionView, titleText: String) {
     
     let registration = UploadedCellRegistration { cell, _, imageLink in
       cell.configure(with: imageLink)
     }
     
     let headerRegistration = HeaderRegistration(elementKind: UICollectionView.elementKindSectionHeader) { supplementaryView, _, _ in
+      supplementaryView.configure(with: titleText)
     }
     
-    setCollectionViewSubject
-      .subscribe(with: self) { owner, collectionView in
-        // dataSource에 cell 등록
-        owner.dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, item in
-          return collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: item)
-        }
-        
-        // dataSource에 headerView도 등록
-        owner.dataSource?.supplementaryViewProvider = .init { collectionView, _, indexPath in
-          return collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
-        }
-      }
-      .disposed(by: disposeBag)
+    // dataSource에 cell 등록
+    dataSource = DataSource(collectionView: collectionView) { collectionView, indexPath, item in
+      return collectionView.dequeueConfiguredReusableCell(using: registration, for: indexPath, item: item)
+    }
+    
+    // dataSource에 headerView도 등록
+    dataSource?.supplementaryViewProvider = .init { collectionView, _, indexPath in
+      return collectionView.dequeueConfiguredReusableSupplementary(using: headerRegistration, for: indexPath)
+    }
   }
   
   /// 초기 Snapshot을 설정합니다. DataSource가 초기화될 시 해당 메서드가 실행됩니다.
@@ -103,6 +119,7 @@ private extension ArchiveUploadViewModel {
   func applyInitialSnapshots() {
     var snapshot = Snapshot()
     snapshot.appendSections([.main])
+    snapshot.appendItems(archivesContents)
     dataSource?.apply(snapshot)
   }
 }
