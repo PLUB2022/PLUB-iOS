1bf7505c6a588446769ad7a169a5682f0399e61e
soobin-k <tnqls27qks@naver.com>
2023-04-23 07:08:56 +0900

    [FEAT] 강퇴하기, 플러버 리스트 UseCase 연결

--- PLUB/Sources/Views/Meeting/Export/ExportViewModel.swift
+++ PLUB/Sources/Views/Meeting/Export/ExportViewModel.swift
@@ -12,6 +12,9 @@ final class ExportViewModel {
   private let disposeBag = DisposeBag()
   private let plubbingID: Int
   
+  private let inquireMeetingMemberUseCase: InquireMeetingMemberUseCase
+  private let exportMeetingMemberUseCase: ExportMeetingMemberUseCase
+  
   // Output
   let accountList: Driver<[AccountInfo]>
   let successExport: Driver<String>
@@ -19,8 +22,15 @@ final class ExportViewModel {
   private let accountListRelay = BehaviorRelay<[AccountInfo]>.init(value: Array.init())
   private let successExportSubject = PublishSubject<String>()
   
-  init(plubbingID: Int) {
+  init(
+    plubbingID: Int,
+    inquireMeetingMemberUseCase: InquireMeetingMemberUseCase,
+    exportMeetingMemberUseCase: ExportMeetingMemberUseCase
+  ) {
     self.plubbingID = plubbingID
+    self.inquireMeetingMemberUseCase = inquireMeetingMemberUseCase
+    self.exportMeetingMemberUseCase = exportMeetingMemberUseCase
+    
     accountList = accountListRelay.asDriver()
     successExport = successExportSubject.asDriver(onErrorDriveWith: .empty())
     
@@ -28,10 +38,11 @@ final class ExportViewModel {
   }
   
   private func fetchMeetingMember() {
-    MeetingService.shared.inquireMeetingMember(plubbingID: plubbingID)
+    inquireMeetingMemberUseCase
+      .execute(plubbingID: plubbingID)
       .withUnretained(self)
-      .subscribe(onNext: { owner, model in
-        owner.accountListRelay.accept(model.accounts)
+      .subscribe(onNext: { owner, accounts in
+        owner.accountListRelay.accept(accounts)
       })
       .disposed(by: disposeBag)
   }
@@ -39,7 +50,8 @@ final class ExportViewModel {
   func exportMember(indexPathRow: Int) {
     let account = accountListRelay.value[indexPathRow]
     let accountID = account.accountId
-    MeetingService.shared.exportMeetingMember(plubbingID: plubbingID, accountID: accountID)
+    exportMeetingMemberUseCase
+      .execute(plubbingID: plubbingID, accountID: accountID)
       .withUnretained(self)
       .subscribe(onNext: { owner, _ in
         let oldValue = owner.accountListRelay.value
