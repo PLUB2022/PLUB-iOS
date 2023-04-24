d0e945e0f9b474916846dbb9430c16128ba29823
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-04-24 16:16:55 +0900

    [ADD] Add dateTime, commentDate in DateFormatterFactory
    서버로부터 댓글 작성 날짜를 파싱하기 위해 두 개의 formatter를 구현했습니다.

--- PLUB/Configuration/Utils/DateFormatterFactory.swift
+++ PLUB/Configuration/Utils/DateFormatterFactory.swift
@@ -16,12 +16,23 @@ enum DateFormatterFactory {
     }
   }
   
+  /// `yyyy. MM. dd`
   static var dateWithDot: DateFormatter {
     dateFormatter.then { $0.dateFormat = "yyyy. MM. dd" }
   }
   
+  /// `yyyy-MM-dd`
   static var dateWithHypen: DateFormatter {
     dateFormatter.then { $0.dateFormat = "yyyy-MM-dd" }
   }
   
+  /// `yyyy-MM-dd HH:mm:ss`
+  static var dateTime: DateFormatter {
+    dateFormatter.then { $0.dateFormat = "yyyy-MM-dd HH:mm:ss" }
+  }
+  
+  /// `yy.MM.dd HH:mm`, 댓글 날짜를 표기하기 위한 DateFormatter
+  static var commentDate: DateFormatter {
+    dateFormatter.then { $0.dateFormat = "yy.MM.dd HH:mm" }
+  }
 }
