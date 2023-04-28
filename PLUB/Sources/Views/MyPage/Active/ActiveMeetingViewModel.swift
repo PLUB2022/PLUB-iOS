a879d30fd10194dd3214e21e0e7ef0d9a84b3f6d
soobin-k <tnqls27qks@naver.com>
2023-04-29 02:59:45 +0900

    [FEAT] 내 투두 조회 API 연결

--- PLUB/Sources/Views/MyPage/Active/ActiveMeetingViewModel.swift
+++ PLUB/Sources/Views/MyPage/Active/ActiveMeetingViewModel.swift
@@ -6,3 +6,60 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+class ActiveMeetingViewModel {
+  private let disposeBag = DisposeBag()
+  private let plubbingID: Int
+  
+  // Output
+  let meetingInfo: Driver<RecruitingModel> // 내 정보 데이터
+  
+  private let meetingInfoSubject = PublishSubject<RecruitingModel>()
+  
+  init(plubbingID: Int) {
+    self.plubbingID = plubbingID
+    meetingInfo = meetingInfoSubject.asDriver(onErrorDriveWith: .empty())
+    
+    fetchMyTodo()
+  }
+  
+  private func fetchMyTodo() {
+    MyPageService.shared
+      .inquireMyTodo(plubbingID: plubbingID, cursorID: 0)
+      .withUnretained(self)
+      .subscribe (onNext: { owner, response in
+        let plubbing = response.plubbingInfo
+        owner.meetingInfoSubject.onNext(
+          RecruitingModel(
+            title: plubbing.name,
+            schedule: owner.createScheduleString(
+              days: plubbing.days,
+              time: plubbing.time
+            ),
+            address: plubbing.address ?? "")
+        )
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  private func createScheduleString(days: [Day], time: String) -> String {
+    let dateStr = days
+      .map { $0.kor }
+      .joined(separator: ", ")
+    
+    let date = DateFormatter().then {
+      $0.dateFormat = "yyyy-MM-dd HH:mm:ss"
+      $0.locale = Locale(identifier: "ko_KR")
+    }.date(from: time) ?? Date()
+
+    let timeStr = DateFormatter().then {
+      $0.dateFormat = "a h시 m분"
+      $0.locale = Locale(identifier: "ko_KR")
+    }.string(from: date)
+    
+    return (dateStr.isEmpty ? "온라인" : dateStr)  + " | " + timeStr
+  }
+}
