688092611a4b60e5f3c390ddc3af174a76f9c797
dlrjswns <leekyunjun@gmail.com>
2023-04-28 16:04:39 +0900

    [CHORE] 모집소개태그에 대한 데이터에 대한 정보만을 가지기위한 코드 수정

--- PLUB/Sources/Views/Home/DetailRecruitmentViewController.swift
+++ PLUB/Sources/Views/Home/DetailRecruitmentViewController.swift
@@ -42,7 +42,7 @@ final class DetailRecruitmentViewController: BaseViewController {
     }
   }
   
-  private var model: DetailRecruitmentModel? {
+  private var model: [String] = [] {
     didSet {
       self.introduceTagCollectionView.reloadData()
     }
@@ -279,22 +279,19 @@ extension DetailRecruitmentViewController: UICollectionViewDelegate, UICollectio
   }
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    guard let model = model else { return 0 }
-    return model.categories.count
+    return model.count
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let model = model else { return UICollectionViewCell() }
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: IntroduceTagCollectionViewCell.identifier, for: indexPath) as? IntroduceTagCollectionViewCell ?? IntroduceTagCollectionViewCell()
-    cell.configureUI(with: model.categories[indexPath.row])
+    cell.configureUI(with: model[indexPath.row])
     return cell
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    guard let model = model else { return .zero }
     let label = UILabel().then {
       $0.font = .caption
-      $0.text = model.categories[indexPath.row]
+      $0.text = model[indexPath.row]
       $0.sizeToFit()
     }
     let size = label.frame.size
