cc51038f82807a47bf0c7d9ec6bdd79dd95f771f
dlrjswns <leekyunjun@gmail.com>
2023-05-02 20:36:49 +0900

    [ADD] 투두리스트 전체조회에 대한 뷰 모델 코드 추가

--- PLUB/Sources/Views/Home/MainPage/ViewModel/TodolistViewModel.swift
+++ PLUB/Sources/Views/Home/MainPage/ViewModel/TodolistViewModel.swift
@@ -5,11 +5,35 @@
 //  Created by 이건준 on 2023/05/02.
 //
 
+import RxSwift
 
 protocol TodolistViewModelType {
-  
+  var selectPlubbingID: AnyObserver<Int> { get }
 }
 
 final class TodolistViewModel: TodolistViewModelType {
   
+  private let disposeBag = DisposeBag()
+  
+  let selectPlubbingID: AnyObserver<Int>
+  
+  init() {
+    let selectingPlubbingID = PublishSubject<Int>()
+    self.selectPlubbingID = selectingPlubbingID.asObserver()
+    
+    let inquireAllTodolist = selectingPlubbingID.flatMap {
+      return TodolistService.shared.inquireAllTodolist(plubbingID: $0, cursorID: 0)
+    }
+    
+    let successInquireAllTodolist = inquireAllTodolist.compactMap { result -> [InquireAllTodolistResponse]? in
+      guard case .success(let response) = result else { return nil }
+      return response.data?.content
+    }
+    
+    successInquireAllTodolist
+      .subscribe(onNext: { model in
+        print("콘텐츠 \(model)")
+      })
+      .disposed(by: disposeBag)
+  }
 }
