a7f55883f28d734288244bb7a976a88da1dbe26e
soobin-k <tnqls27qks@naver.com>
2023-05-04 00:35:38 +0900

    [ADD] `a h시 m분` dateFormat 추가

--- PLUB/Configuration/Utils/DateFormatterFactory.swift
+++ PLUB/Configuration/Utils/DateFormatterFactory.swift
@@ -35,4 +35,9 @@ enum DateFormatterFactory {
   static var commentDate: DateFormatter {
     dateFormatter.then { $0.dateFormat = "yy.MM.dd HH:mm" }
   }
+  
+  /// `a h시 m분`
+  static var koreanTime: DateFormatter {
+    dateFormatter.then { $0.dateFormat = "a h시 m분" }
+  }
 }
