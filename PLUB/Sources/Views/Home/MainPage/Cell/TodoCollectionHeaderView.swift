0517390c20a79b67c0dd6e7130d5807a5b2da688
dlrjswns <leekyunjun@gmail.com>
2023-05-03 19:29:49 +0900

    [ADD] 투두리스트 날짜를 위한 헤더뷰 추가

---
+++ PLUB/Sources/Views/Home/MainPage/Cell/TodoCollectionHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  TodoCollectionHeaderView.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/05/03.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+struct TodoCollectionHeaderViewModel {
+  let isToday: Bool
+  let date: String
+}
+
+final class TodoCollectionHeaderView: UICollectionReusableView {
+  
+  static let identifier = "TodoCollectionHeaderView"
+  
+  private let dateLabel = UILabel().then {
+    $0.textColor = .black
+    $0.font = .body1
+    $0.sizeToFit()
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    configureUI()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  private func configureUI() {
+    addSubview(dateLabel)
+    dateLabel.snp.makeConstraints {
+      $0.centerY.leading.equalToSuperview()
+    }
+  }
+  
+  func configureUI(with model: TodoCollectionHeaderViewModel) {
+    dateLabel.textColor = model.isToday ? .black : .mediumGray
+    dateLabel.text = model.isToday ? "\(model.date)(오늘)" : model.date
+  }
+}
