12baaf6a35f5d49b56bc0268cc2d6994f854b77a
dlrjswns <leekyunjun@gmail.com>
2023-05-06 17:18:07 +0900

    [ADD] TodolistService 함수 주석 달기

--- PLUB/Sources/Network/Services/TodolistService.swift
+++ PLUB/Sources/Network/Services/TodolistService.swift
@@ -14,6 +14,11 @@ final class TodolistService: BaseService {
 }
 
 extension TodolistService {
+  
+  /// 전체타임라인을 조회합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - cursorID: 타임라인 페이징을 위한 커서 ID
   func inquireAllTodoTimeline(plubbingID: Int, cursorID: Int = 0) -> PLUBResult<PaginatedDataResponse<InquireAllTodoTimelineResponse>> {
     sendRequest(
       TodolistRouter.inquireAllTodoTimeline(plubbingID, cursorID),
@@ -21,22 +26,43 @@ extension TodolistService {
     )
   }
   
+  /// 투두 상세조회합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - timelineID: 투두에 해당하는 타임라인 ID
   func inquireTodolist(plubbingID: Int, timelineID: Int) -> Observable<InquireTodolistResponse> {
     sendObservableRequest(TodolistRouter.inquireTodolist(plubbingID, timelineID))
   }
   
+  /// 투두리스트 완료요청합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - todolistID: 투두리스트 ID
   func completeTodolist(plubbingID: Int, todolistID: Int) -> Observable<CompleteProofTodolistResponse> {
     sendObservableRequest(TodolistRouter.completeTodolist(plubbingID, todolistID))
   }
   
+  /// 투두리스트 완료취소요청합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - todolistID: 투두리스트 ID
   func cancelCompleteTodolist(plubbingID: Int, todolistID: Int) -> Observable<CompleteProofTodolistResponse> {
     sendObservableRequest(TodolistRouter.cancelCompleteTodolist(plubbingID, todolistID))
   }
   
+  /// 투두리스트 인증요청합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - todolistID: 투두리스트 ID
+  ///   - request: 인증을 위한 이미지 요청모델
   func proofTodolist(plubbingID: Int, todolistID: Int, request: ProofTodolistRequest) -> Observable<CompleteProofTodolistResponse> {
     sendObservableRequest(TodolistRouter.proofTodolist(plubbingID, todolistID, request))
   }
   
+  /// 투두리스트 좋아요/좋아요 취소 요청합니다.
+  /// - Parameters:
+  ///   - plubbingID: 플럽 모임 ID
+  ///   - timelineID: 타임라인 ID
   func likeTodolist(plubbingID: Int, timelineID: Int) -> Observable<LikeTodolistResponse> {
     sendObservableRequest(TodolistRouter.likeTodolist(plubbingID, timelineID))
   }
