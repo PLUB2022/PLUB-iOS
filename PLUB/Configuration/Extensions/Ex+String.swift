79c2ace8fc6fe1634a8308bfe2d0d11d52f79489
dlrjswns <leekyunjun@gmail.com>
2023-05-07 21:57:43 +0900

    [ADD] pr리뷰에 따른 공통함수 주석 추가

--- PLUB/Configuration/Extensions/Ex+String.swift
+++ PLUB/Configuration/Extensions/Ex+String.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension String {
+  
+  /// 라벨 취소선을 설정하는 함수입니다.
   func strikeThrough() -> NSAttributedString {
     let attributeString = NSMutableAttributedString(string: self)
     attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
