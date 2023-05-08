ef7abfc964330c22d7432bd68a12da0920a3764a
dlrjswns <leekyunjun@gmail.com>
2023-05-08 17:17:32 +0900

    [FEAT] 투두추가를 위한 UI 및 모델 정의

--- PLUB/Sources/Views/Home/MainPage/Component/AddTodoView.swift
+++ PLUB/Sources/Views/Home/MainPage/Component/AddTodoView.swift
@@ -7,49 +7,23 @@
 
 import UIKit
 
+import RxSwift
 import SnapKit
 import Then
 
-final class TodoView: UIView {
-  
-  private let emptyCheckView = UIView().then {
-    $0.layer.borderWidth = 1
-    $0.layer.borderColor = UIColor.gray.cgColor
-    $0.layer.cornerRadius = 3
-    $0.layer.masksToBounds = true
-  }
-  
-  private let todoTextField = UITextField().then {
-    $0.placeholder = "새로운 TO-DO 추가하기"
-  }
-  
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    configureUI()
-  }
-  
-  required init(coder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
-  }
-  
-  override func layoutSubviews() {
-    super.layoutSubviews()
-    todoTextField.addUnderline(color: .deepGray)
-  }
-  
-  private func configureUI() {
-    [emptyCheckView, todoTextField].forEach { addSubview($0) }
-    
-    emptyCheckView.snp.makeConstraints {
-      $0.size.equalTo(18)
-      $0.leading.directionalVerticalEdges.equalToSuperview().inset(7)
-    }
-    
-    todoTextField.snp.makeConstraints {
-      $0.leading.equalTo(emptyCheckView.snp.trailing).offset(7)
-      $0.directionalVerticalEdges.trailing.equalToSuperview()
-    }
-  }
+enum TodoViewType {
+  case input // 투두입력할 수 있는 타입
+  case todo // 입력한 투두에 대한 타입
+}
+
+struct TodoViewModel {
+  let isChecked: Bool
+  let content: String
+}
+
+struct AddTodoViewModel {
+  let date: String
+  let todoViewModel: [TodoViewModel]
 }
 
 final class AddTodoView: UIView {
@@ -86,7 +60,104 @@ final class AddTodoView: UIView {
     
     todoContainerView.addArrangedSubview(dateLabel)
     
-    let todoView = TodoView()
+    let todoView = TodoView(type: .input)
     todoContainerView.addArrangedSubview(todoView)
+  }
+  
+  func configureUI(with model: AddTodoViewModel) {
+    dateLabel.text = model.date
+    model.todoViewModel.forEach { model in
+      let todoView = TodoView(type: .todo)
+      todoView.configureUI(with: .init(
+        isChecked: model.isChecked,
+        content: model.content)
+      )
+      todoContainerView.addArrangedSubview(todoView)
+    }
+  }
+}
+
+extension AddTodoView {
+  final class TodoView: UIView {
+    
+    private let disposeBag = DisposeBag()
+    private let type: TodoViewType
+    
+    private lazy var emptyCheckView = UIView().then {
+      $0.layer.borderWidth = 1
+      $0.layer.borderColor = UIColor.gray.cgColor
+      $0.layer.cornerRadius = 3
+      $0.layer.masksToBounds = true
+    }
+    
+    private lazy var contentLabel = UILabel().then {
+      $0.text = "투두리스트목록"
+    }
+    
+    private lazy var checkBoxButton = CheckBoxButton(type: .none)
+    
+    private let todoTextField = UITextField().then {
+      $0.placeholder = "새로운 TO-DO 추가하기"
+    }
+    
+    init(type: TodoViewType) {
+      self.type = type
+      super.init(frame: .zero)
+      configureUI()
+      bind()
+    }
+    
+    required init(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+      super.layoutSubviews()
+      if type == .input {
+        todoTextField.addUnderline(color: .deepGray)
+      }
+    }
+    
+    private func configureUI() {
+      switch type {
+      case .input:
+        [emptyCheckView, todoTextField].forEach { addSubview($0) }
+        
+        emptyCheckView.snp.makeConstraints {
+          $0.size.equalTo(18)
+          $0.leading.directionalVerticalEdges.equalToSuperview().inset(7)
+        }
+        
+        todoTextField.snp.makeConstraints {
+          $0.leading.equalTo(emptyCheckView.snp.trailing).offset(7)
+          $0.directionalVerticalEdges.trailing.equalToSuperview()
+        }
+      case .todo:
+        [checkBoxButton, contentLabel].forEach { addSubview($0) }
+        checkBoxButton.snp.makeConstraints {
+          $0.size.equalTo(18)
+          $0.leading.directionalVerticalEdges.equalToSuperview().inset(7)
+        }
+        
+        contentLabel.snp.makeConstraints {
+          $0.leading.equalTo(checkBoxButton.snp.trailing).offset(7)
+          $0.directionalVerticalEdges.equalToSuperview()
+          $0.trailing.lessThanOrEqualToSuperview()
+        }
+      }
+    }
+    
+    func configureUI(with model: TodoViewModel) {
+      contentLabel.text = model.content
+      checkBoxButton.isChecked = model.isChecked
+    }
+    
+    func bind() {
+      checkBoxButton.rx.isChecked
+        .subscribe(with: self) { owner, isChecked in
+          owner.contentLabel.attributedText = isChecked ? owner.contentLabel.text?.strikeThrough() : NSAttributedString(string: owner.contentLabel.text ?? "")
+        }
+        .disposed(by: disposeBag)
+    }
   }
 }
