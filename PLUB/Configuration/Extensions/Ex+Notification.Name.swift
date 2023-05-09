e9210cd6fe78a02ac78f0b2a1e861e73fd8404a7
soobin-k <tnqls27qks@naver.com>
2023-05-10 01:54:39 +0900

    [ADD] Notification.Name extension 추가

---
+++ PLUB/Configuration/Extensions/Ex+Notification.Name.swift
@@ -0,0 +1,12 @@
+//
+//  Ex+Notification.Name.swift
+//  PLUB
+//
+//  Created by 김수빈 on 2023/05/10.
+//
+
+import Foundation
+
+extension Notification.Name {
+  static let refreshMeeting = Notification.Name(rawValue: "refreshMeeting")
+}
