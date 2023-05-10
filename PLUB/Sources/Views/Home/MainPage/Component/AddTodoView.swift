c64e12c60c92d1ea714ba8af75d7e0f5e2597016
dlrjswns <leekyunjun@gmail.com>
2023-05-10 20:56:44 +0900

    [MOVE] AddTodoView클래스 파일 이동 및 델리게이트 선언

--- PLUB/Sources/Views/Home/MainPage/Component/AddTodoView.swift
+++ PLUB/Sources/Views/Home/MainPage/Component/AddTodoView.swift
@@ -16,17 +16,18 @@ enum TodoViewType {
   case todo // 입력한 투두에 대한 타입
 }
 
-struct TodoViewModel {
-  let isChecked: Bool
-  let content: String
-}
-
 struct AddTodoViewModel {
   let todoViewModel: [TodoViewModel]
 }
 
+protocol AddTodoViewDelegate: AnyObject {
+  func whichCreateTodoRequest(request: CreateTodoRequest)
+}
+
 final class AddTodoView: UIView {
   
+  weak var delegate: AddTodoViewDelegate?
+  private var date: Date?
   private(set) var completionHandler: ((Date) -> Void)?
   
   private let todoContainerView = UIStackView().then {
@@ -45,6 +46,7 @@ final class AddTodoView: UIView {
     super.init(frame: frame)
     configureUI()
     completionHandler = { [weak self] date in
+      self?.date = date
       let dateString = DateFormatterFactory.todolistDate.string(from: date)
       let isToday = Calendar.current.isDateInToday(date)
       self?.dateLabel.text = isToday ? "\(dateString) (오늘)" : dateString
@@ -76,6 +78,7 @@ final class AddTodoView: UIView {
   func configureUI(with model: AddTodoViewModel) {
     model.todoViewModel.forEach { model in
       let todoView = TodoView(type: .todo)
+      todoView.delegate = self
       todoView.configureUI(with: .init(
         isChecked: model.isChecked,
         content: model.content)
@@ -85,110 +88,10 @@ final class AddTodoView: UIView {
   }
 }
 
-extension AddTodoView {
-  final class TodoView: UIView {
-    
-    private let disposeBag = DisposeBag()
-    private let type: TodoViewType
-    
-    private lazy var emptyCheckView = UIView().then {
-      $0.layer.borderWidth = 1
-      $0.layer.borderColor = UIColor.lightGray.cgColor
-      $0.layer.cornerRadius = 3
-      $0.layer.masksToBounds = true
-    }
-    
-    private lazy var contentLabel = UILabel().then {
-      $0.text = "투두리스트목록"
-    }
-    
-    private lazy var checkBoxButton = CheckBoxButton(type: .none)
-    
-    private let todoTextField = UITextField().then {
-      $0.placeholder = "새로운 TO-DO 추가하기"
-      $0.layer.shouldRasterize = true
-    }
-    
-    private lazy var moreButton = UIButton().then {
-      $0.setImage(UIImage(named: "meatballMenuBlack"), for: .normal)
-    }
-    
-    private lazy var bottomLineView = UIView().then {
-      $0.backgroundColor = .lightGray
-    }
-    
-    init(type: TodoViewType) {
-      self.type = type
-      super.init(frame: .zero)
-      configureUI()
-      bind()
-    }
-    
-    required init(coder: NSCoder) {
-      fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func configureUI() {
-      switch type {
-      case .input:
-        [emptyCheckView, todoTextField].forEach { addSubview($0) }
-        todoTextField.addSubview(bottomLineView)
-        
-        emptyCheckView.snp.makeConstraints {
-          $0.size.equalTo(18)
-          $0.leading.directionalVerticalEdges.equalToSuperview().inset(7)
-        }
-        
-        todoTextField.snp.makeConstraints {
-          $0.leading.equalTo(emptyCheckView.snp.trailing).offset(7)
-          $0.directionalVerticalEdges.trailing.equalToSuperview()
-        }
-        
-        bottomLineView.snp.makeConstraints {
-          $0.directionalHorizontalEdges.bottom.equalToSuperview()
-          $0.height.equalTo(1)
-        }
-        
-      case .todo:
-        [checkBoxButton, contentLabel].forEach { addSubview($0) }
-        contentLabel.addSubview(bottomLineView)
-        contentLabel.addSubview(moreButton)
-        checkBoxButton.snp.makeConstraints {
-          $0.size.equalTo(18)
-          $0.leading.directionalVerticalEdges.equalToSuperview().inset(7)
-        }
-        
-        contentLabel.snp.makeConstraints {
-          $0.leading.equalTo(checkBoxButton.snp.trailing).offset(7)
-          $0.directionalVerticalEdges.equalToSuperview()
-          $0.trailing.equalToSuperview()
-        }
-        
-        bottomLineView.snp.makeConstraints {
-          $0.width.equalToSuperview()
-          $0.height.equalTo(1)
-          $0.bottom.equalToSuperview()
-        }
-        
-        moreButton.snp.makeConstraints {
-          $0.size.equalTo(32)
-          $0.directionalVerticalEdges.trailing.equalToSuperview()
-        }
-      }
-    }
-    
-    func configureUI(with model: TodoViewModel) {
-      contentLabel.text = model.content
-      checkBoxButton.isChecked = model.isChecked
-      contentLabel.attributedText = model.isChecked ? contentLabel.text?.strikeThrough() : NSAttributedString(string: contentLabel.text ?? "")
-    }
-    
-    private func bind() {
-      checkBoxButton.rx.isChecked
-        .subscribe(with: self) { owner, isChecked in
-          owner.contentLabel.attributedText = isChecked ? owner.contentLabel.text?.strikeThrough() : NSAttributedString(string: owner.contentLabel.text ?? "")
-        }
-        .disposed(by: disposeBag)
-    }
+extension AddTodoView: TodoViewDelegate {
+  func whichTodoContent(content: String) {
+    guard let date = self.date else { return }
+    let dateString = DateFormatterFactory.dateWithHypen.string(from: date)
+    delegate?.whichCreateTodoRequest(request: .init(content: content, date: dateString))
   }
 }
