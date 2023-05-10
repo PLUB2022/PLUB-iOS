ecc80f6ed5dc026e8a9eb8da0f8d2b238947e73a
dlrjswns <leekyunjun@gmail.com>
2023-05-10 20:58:34 +0900

    [FEAT] 투두생성을 위한 뷰 모델 기능 추가

--- PLUB/Sources/Views/Home/MainPage/ViewModel/AddTodoViewModel.swift
+++ PLUB/Sources/Views/Home/MainPage/ViewModel/AddTodoViewModel.swift
@@ -5,12 +5,40 @@
 //  Created by 이건준 on 2023/05/10.
 //
 
-import Foundation
+import RxSwift
 
-protocol AddTodoViewModelType {
-  
+protocol AddTodoListViewModelType {
+  var whichCreateTodoRequest: AnyObserver<CreateTodoRequest> { get }
 }
 
-final class AddTodoViewModel: AddTodoViewModelType {
+final class AddTodoListViewModel: AddTodoListViewModelType {
   
+  private let disposeBag = DisposeBag()
+  
+  private(set) var plubbingID: Int = 0
+  private let createTodoRequst = PublishSubject<CreateTodoRequest>()
+  
+  init(plubbingID: Int) {
+    self.plubbingID = plubbingID
+    tryCreateTodo()
+  }
+  
+  private func tryCreateTodo() {
+    let requestCreateTodo = createTodoRequst
+      .withUnretained(self)
+      .flatMapLatest { owner, request in
+        TodolistService.shared.createTodo(plubbingID: owner.plubbingID, request: request)
+      }
+    
+    requestCreateTodo.subscribe(onNext: { response in
+      Log.debug("투두생성응답값 \(response)")
+    })
+      .disposed(by: disposeBag)
+  }
+}
+
+extension AddTodoListViewModel {
+  var whichCreateTodoRequest: AnyObserver<CreateTodoRequest> {
+    createTodoRequst.asObserver()
+  }
 }
