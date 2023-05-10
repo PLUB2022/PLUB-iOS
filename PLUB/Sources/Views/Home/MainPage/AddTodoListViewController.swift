98779299d39f9e8025dffe35bcbe4a5280acace0
dlrjswns <leekyunjun@gmail.com>
2023-05-10 20:56:07 +0900

    [ADD] 투두생성화면관련 뷰모델 연동코드 추가

--- PLUB/Sources/Views/Home/MainPage/AddTodoListViewController.swift
+++ PLUB/Sources/Views/Home/MainPage/AddTodoListViewController.swift
@@ -42,12 +42,22 @@ final class AddTodoListViewController: BaseViewController {
     $0.appearance.titleWeekendColor = .red
   }
   
-  private let addTodoView = AddTodoView().then {
+  private lazy var addTodoView = AddTodoView().then {
     $0.backgroundColor = .subMain
     $0.layer.borderWidth = 1
     $0.layer.borderColor = UIColor.main.cgColor
     $0.layer.cornerRadius = 10
     $0.layer.masksToBounds = true
+    $0.delegate = self
+  }
+  
+  init(viewModel: AddTodoListViewModelType) {
+    self.viewModel = viewModel
+    super.init(nibName: nil, bundle: nil)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
   }
   
   override func setupStyles() {
