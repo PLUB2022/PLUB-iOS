b695ae508340dbe37e8295f9f886fe2658a7a81d
dlrjswns <leekyunjun@gmail.com>
2023-05-10 19:29:56 +0900

    [ADD] AddTodoViewModel 파일 추가

---
+++ PLUB/Sources/Views/Home/MainPage/ViewModel/AddTodoViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  AddTodoViewModel.swift
+//  PLUB
+//
+//  Created by 이건준 on 2023/05/10.
+//
+
+import Foundation
+
+protocol AddTodoViewModelType {
+  
+}
+
+final class AddTodoViewModel: AddTodoViewModelType {
+  
+}
