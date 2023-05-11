d4477fa78c3fbbe9e78a60e2c33a422c7cbc2d8f
dlrjswns <leekyunjun@gmail.com>
2023-05-11 15:04:28 +0900

    [FEAT] 날짜별 투두리스트조회에 대한 데이터바인딩코드 작성

--- PLUB/Sources/Views/Home/MainPage/ViewModel/AddTodoViewModel.swift
+++ PLUB/Sources/Views/Home/MainPage/ViewModel/AddTodoViewModel.swift
@@ -5,29 +5,42 @@
 //  Created by 이건준 on 2023/05/10.
 //
 
+import Foundation
+
 import RxSwift
+import RxCocoa
+import RxRelay
 
 protocol AddTodoListViewModelType {
   var whichCreateTodoRequest: AnyObserver<CreateTodoRequest> { get }
+  var whichInquireDate: AnyObserver<Date> { get }
+  var selectPlubbingID: AnyObserver<Int> { get }
+  
+  var todolistModelByDate: Driver<AddTodoViewModel> { get }
 }
 
 final class AddTodoListViewModel: AddTodoListViewModelType {
   
   private let disposeBag = DisposeBag()
   
-  private(set) var plubbingID: Int = 0
+  private let whichPlubbingID = PublishSubject<Int>()
   private let createTodoRequst = PublishSubject<CreateTodoRequest>()
+  private let inquireDate = PublishSubject<Date>()
+  private let todolistModelByCurrentDate = PublishRelay<AddTodoViewModel>()
   
-  init(plubbingID: Int) {
-    self.plubbingID = plubbingID
+  init() {
     tryCreateTodo()
+    tryInquireTodolistByDate()
   }
   
   private func tryCreateTodo() {
-    let requestCreateTodo = createTodoRequst
-      .withUnretained(self)
-      .flatMapLatest { owner, request in
-        TodolistService.shared.createTodo(plubbingID: owner.plubbingID, request: request)
+    let requestCreateTodo =
+    Observable.combineLatest(
+      whichPlubbingID,
+      createTodoRequst
+    )
+      .flatMapLatest { plubbingID, request in
+        TodolistService.shared.createTodo(plubbingID: plubbingID, request: request)
       }
     
     requestCreateTodo.subscribe(onNext: { response in
@@ -35,10 +48,43 @@ final class AddTodoListViewModel: AddTodoListViewModelType {
     })
       .disposed(by: disposeBag)
   }
+  
+  private func tryInquireTodolistByDate() {
+    let inquireTodolistByDate =
+    Observable.combineLatest(
+      whichPlubbingID,
+      inquireDate.map { DateFormatterFactory.dateWithHypen.string(from: $0) }
+    )
+      .flatMapLatest { plubbingID, date in
+        return TodolistService.shared.inquireTodolistByDate(
+          plubbingID: plubbingID,
+          todoDate: date
+        )
+      }
+    
+    inquireTodolistByDate
+      .map { AddTodoViewModel(response: $0) }
+      .bind(to: todolistModelByCurrentDate)
+      .disposed(by: disposeBag)
+  }
 }
 
 extension AddTodoListViewModel {
+  
+  var selectPlubbingID: AnyObserver<Int> {
+    whichPlubbingID.asObserver()
+  }
+  
   var whichCreateTodoRequest: AnyObserver<CreateTodoRequest> {
     createTodoRequst.asObserver()
+  }
+  
+  var whichInquireDate: AnyObserver<Date> {
+    inquireDate.asObserver()
+  }
+  
+  var todolistModelByDate: Driver<AddTodoViewModel> {
+    todolistModelByCurrentDate
+      .asDriver(onErrorDriveWith: .empty())
   }
 }
