08f66fc88b3614cc6fcecaf3da1b4689821f1c2a
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-05-16 18:41:35 +0900

    [RENAME] NoticeViewController to AnnouncementViewController

---
+++ PLUB/Sources/Views/Home/Announcement/AnnouncementViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AnnouncementViewController.swift
+//  PLUB
+//
+//  Created by 홍승현 on 2023/05/16.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class AnnouncementViewController: BaseViewController {
+  
+}
