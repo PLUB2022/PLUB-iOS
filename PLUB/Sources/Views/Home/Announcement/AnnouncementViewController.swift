7ea12979945fd5f107634f977dade810c1de48cb
SeungHyeon Hong <whi7ehyun@gmail.com>
2023-05-16 19:03:48 +0900

    [FEAT] Implement Announcement's UI

--- PLUB/Sources/Views/Home/Announcement/AnnouncementViewController.swift
+++ PLUB/Sources/Views/Home/Announcement/AnnouncementViewController.swift
@@ -12,4 +12,71 @@ import Then
 
 final class AnnouncementViewController: BaseViewController {
   
+  // MARK: - UI Components
+  
+  // MARK: Header
+  
+  private let headerStackView = UIStackView().then {
+    $0.alignment = .center
+  }
+  
+  private let gradientLayer = CAGradientLayer().then {
+    $0.locations = [0.7]
+    $0.colors = [UIColor.background.cgColor, UIColor.background.withAlphaComponent(0).cgColor]
+  }
+  
+  private let titleLabel = UILabel().then {
+    $0.text = "공지사항"
+    $0.font = .h3
+  }
+  
+  private let uploadButton = UIButton(
+    configuration: .plubFilledButton(
+      title: "+ 새 글 작성",
+      contentInsets: .init(top: 5, leading: 8, bottom: 5, trailing: 8)
+    )
+  )
+  
+  // MARK: Body
+  
+  private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: .init()).then {
+    $0.collectionViewLayout = UICollectionViewCompositionalLayout.list(using: .init(appearance: .insetGrouped))
+    $0.showsVerticalScrollIndicator = false
+  }
+  
+  // MARK: - Life Cycles
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    var frame = headerStackView.bounds
+    frame.size.height += 32
+    gradientLayer.frame = frame
+  }
+  
+  // MARK: - Configurations
+  
+  override func setupLayouts() {
+    super.setupLayouts()
+    view.addSubview(collectionView)
+    view.addSubview(headerStackView)
+    
+    headerStackView.layer.addSublayer(gradientLayer)
+    [titleLabel, uploadButton].forEach {
+      headerStackView.addArrangedSubview($0)
+    }
+  }
+  
+  override func setupConstraints() {
+    super.setupConstraints()
+    headerStackView.snp.makeConstraints {
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+    }
+    
+    collectionView.snp.makeConstraints {
+      $0.directionalHorizontalEdges.equalToSuperview().inset(16)
+      $0.top.equalTo(headerStackView.snp.top)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
 }
