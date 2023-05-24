5d29c400947019efc3eaae571d0b54aed68b25aa
dlrjswns <leekyunjun@gmail.com>
2023-05-24 23:40:01 +0900

    [CHORE] 전체 스크롤을 위한 컬렉션뷰로 코드 수정

--- PLUB/Sources/Views/Home/MainPage/TodolistViewController.swift
+++ PLUB/Sources/Views/Home/MainPage/TodolistViewController.swift
@@ -26,6 +26,7 @@ final class TodolistViewController: BaseViewController {
   weak var delegate: TodolistDelegate?
   
   private let plubbingID: Int
+  private let goal: String
   
   private var model: [TodolistModel] = [] {
     didSet {
@@ -39,38 +40,17 @@ final class TodolistViewController: BaseViewController {
     }
   }
   
-  private let scrollView = UIScrollView().then {
-    $0.showsVerticalScrollIndicator = true
-    $0.showsHorizontalScrollIndicator = false
-    $0.isScrollEnabled = true
-    $0.alwaysBounceVertical = true
-  }
-  
-  private let scrollContainerView = UIView().then {
-    $0.backgroundColor = .clear
-  }
-  
-  private let titleLabel = UILabel().then {
-    $0.font = .appFont(family: .nanum, size: 32)
-    $0.textAlignment = .center
-    $0.numberOfLines = 0
-    $0.sizeToFit()
-  }
-  
-  private let goalBackgroundView = UIView().then {
-    $0.backgroundColor = .subMain
-  }
-  
   private lazy var todoCollectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
     $0.minimumLineSpacing = 8
+    $0.scrollDirection = .vertical
   }).then {
     $0.backgroundColor = .background
     $0.register(TodoCollectionViewCell.self, forCellWithReuseIdentifier: TodoCollectionViewCell.identifier)
     $0.register(TodoCollectionHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: TodoCollectionHeaderView.identifier)
+    $0.register(TodoGoalHeaderView.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: TodoGoalHeaderView.identifier)
     $0.delegate = self
     $0.dataSource = self
     $0.contentInset = UIEdgeInsets(top: .zero, left: 16, bottom: .zero, right: 16)
-    $0.isScrollEnabled = false
     $0.showsVerticalScrollIndicator = false
     $0.showsHorizontalScrollIndicator = false
   }
@@ -78,7 +58,7 @@ final class TodolistViewController: BaseViewController {
   init(plubbingID: Int, goal: String, viewModel: TodolistViewModelType = TodolistViewModel()) {
     self.viewModel = viewModel
     self.plubbingID = plubbingID
-    titleLabel.text = goal
+    self.goal = goal
     super.init(nibName: nil, bundle: nil)
     bind(plubbingID: plubbingID)
   }
@@ -95,38 +75,14 @@ final class TodolistViewController: BaseViewController {
   
   override func setupLayouts() {
     super.setupLayouts()
-    view.addSubview(scrollView)
-    scrollView.addSubview(scrollContainerView)
-    [goalBackgroundView, titleLabel, todoCollectionView].forEach { scrollContainerView.addSubview($0) }
+    [todoCollectionView].forEach { view.addSubview($0) }
   }
   
   override func setupConstraints() {
     super.setupConstraints()
     
-    scrollView.snp.makeConstraints {
-      $0.directionalEdges.equalToSuperview()
-    }
-    
-    scrollContainerView.snp.makeConstraints {
-      $0.directionalEdges.width.equalToSuperview()
-    }
-    
-    goalBackgroundView.snp.makeConstraints {
-      $0.top.equalToSuperview().inset(40)
-      $0.centerX.equalToSuperview()
-      $0.width.equalTo(187)
-      $0.height.equalTo(19)
-    }
-    
-    titleLabel.snp.makeConstraints {
-      $0.top.equalToSuperview().inset(24)
-      $0.directionalHorizontalEdges.equalToSuperview()
-    }
-    
     todoCollectionView.snp.makeConstraints {
-      $0.top.equalTo(goalBackgroundView.snp.bottom).offset(26)
-      $0.directionalHorizontalEdges.bottom.equalToSuperview()
-      $0.height.equalTo(UIScreen.main.bounds.height - Device.navigationBarHeight - 32 - 24 - 21 - 40)
+      $0.directionalEdges.equalToSuperview()
     }
   }
   
@@ -168,27 +124,41 @@ final class TodolistViewController: BaseViewController {
 
 extension TodolistViewController: UICollectionViewDelegate, UICollectionViewDataSource {
   func numberOfSections(in collectionView: UICollectionView) -> Int {
-    return model.count
+    return model.count + 1
   }
   
   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    if section == 0 {
+      return 0
+    }
     return 1
   }
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    if indexPath.section == 0 {
+      return UICollectionViewCell()
+    }
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TodoCollectionViewCell.identifier, for: indexPath) as? TodoCollectionViewCell ?? TodoCollectionViewCell()
-    cell.configureUI(with: model[indexPath.section].cellModel)
+    cell.configureUI(with: model[indexPath.section - 1].cellModel)
     cell.delegate = self
     return cell
   }
   
   func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+    if indexPath.section == 0 {
+      let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: TodoGoalHeaderView.identifier, for: indexPath) as? TodoGoalHeaderView ?? TodoGoalHeaderView()
+      header.configureUI(with: goal)
+      return header
+    }
     let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: TodoCollectionHeaderView.identifier, for: indexPath) as? TodoCollectionHeaderView ?? TodoCollectionHeaderView()
-    header.configureUI(with: model[indexPath.section].headerModel)
+    header.configureUI(with: model[indexPath.section - 1].headerModel)
     return header
   }
   
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    if section == 0 {
+      return CGSize(width: collectionView.frame.width, height: 85)
+    }
     let width: CGFloat = collectionView.frame.width
     let height: CGFloat = 8 + 21 + 8
     return CGSize(width: width, height: height)
@@ -197,12 +167,15 @@ extension TodolistViewController: UICollectionViewDelegate, UICollectionViewData
 
 extension TodolistViewController: UICollectionViewDelegateFlowLayout {
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    if indexPath.section == 0 {
+      return .zero
+    }
     return TodoCollectionViewCell.estimatedCommentCellSize(
       CGSize(
         width: view.bounds.width - 32,
         height: UIView.layoutFittingCompressedSize.height
       ),
-      model: model[indexPath.section].cellModel
+      model: model[indexPath.section - 1].cellModel
     )
   }
 }
