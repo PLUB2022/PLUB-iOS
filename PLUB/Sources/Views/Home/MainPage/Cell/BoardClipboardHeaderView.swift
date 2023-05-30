792337107ae4f8322f155a99c49eff1851dfe0e7
dlrjswns <leekyunjun@gmail.com>
2023-05-30 15:42:55 +0900

    [FIX] 클립보드리스트 분할되어보이는 문제 해결

--- PLUB/Sources/Views/Home/MainPage/Cell/BoardClipboardHeaderView.swift
+++ PLUB/Sources/Views/Home/MainPage/Cell/BoardClipboardHeaderView.swift
@@ -77,6 +77,7 @@ final class BoardClipboardHeaderView: UICollectionReusableView {
   override func prepareForReuse() {
     super.prepareForReuse()
     entireStackView.subviews.forEach { $0.removeFromSuperview() }
+    verticalStackView.subviews.forEach { $0.removeFromSuperview() }
   }
   
   private func bind() {
